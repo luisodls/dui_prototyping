0111763eb9a14aaf7b21758f8bddb60b822600cc
luisodls <luis.fuentes-montero@diamond.ac.uk>
2019-07-11 17:39:43 +0100

    renaming some vars

--- lui_testing/transcrypt_toys/test02/hello.py
+++ lui_testing/transcrypt_toys/test02/hello.py
@@ -5,22 +5,22 @@ class SolarSystem:
     def __init__ (self):
         self.reset_num()
 
-    def count_01 (self):
+    def counter (self):
         self.counting += 1
         self.update_count()
 
-    def explain (self):
-        document.getElementById ('explain').innerHTML = "reset ..."
+    def reset_but (self):
+        document.getElementById ('reset_but').innerHTML = "reset ..."
         self.reset_num()
         self.update_count()
-        document.getElementById ('explain').innerHTML = "done ..."
+        document.getElementById ('reset_but').innerHTML = "done ..."
 
     def reset_num(self):
         self.counting = 0
 
     def update_count(self):
         str_counting = "Number of planets = " + str(self.counting) + "..."
-        document.getElementById ('count_01') .innerHTML = str_counting
+        document.getElementById ('counter') .innerHTML = str_counting
 
 
 
