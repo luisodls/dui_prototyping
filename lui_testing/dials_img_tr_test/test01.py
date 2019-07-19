baa97e8ec2dfb02b434888ffaee37f4dd85b251e
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2019-07-19 13:54:58 +0100

    some first test

--- lui_testing/dials_img_tr_test/test01.py
+++ lui_testing/dials_img_tr_test/test01.py
@@ -1,13 +1,33 @@
 from dials.algorithms.spot_finding.threshold import DispersionThresholdStrategy
 from dxtbx.datablock import DataBlockFactory
-
-
+from dxtbx.model.experiment_list import ExperimentListFactory
 
 n_json_file_path = "/tmp/dui_run/dui_files/1_datablock.json"
 datablocks = DataBlockFactory.from_json_file(n_json_file_path)
 # TODO check length of datablock for safety
 datablock = datablocks[0]
 my_sweep = datablock.extract_sweeps()[0]
+img_arr = my_sweep.get_raw_data(img_pos)[pan_num].as_double()
+
+
+###############################################################################
+
+experiments = ExperimentListFactory.from_json_file(
+                n_json_file_path, check_format=False
+            )
+
+imageset_tmp = experiments.imagesets()[0]
+mask_file = imageset_tmp.external_lookup.mask.filename
+
+pick_file = open(mask_file, "rb")
+mask_tup_obj = pickle.load(pick_file)
+pick_file.close()
+
+mask_flex = mask_tup_obj[0]
+mask_np_arr = mask_flex.as_numpy_array()
+dat.np_mask = mask_np_arr
+
+
 
 tmp_off = '''
 algorithm = DispersionThresholdStrategy(
