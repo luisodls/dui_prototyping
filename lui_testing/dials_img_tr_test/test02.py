9d699d914ca7dd39d90caf4ee45176cfe8743341
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2019-07-19 16:31:37 +0100

    adding one more test ... broken for now

---
+++ lui_testing/dials_img_tr_test/test02.py
@@ -0,0 +1,85 @@
+from dials.algorithms.spot_finding.threshold import DispersionThresholdStrategy
+from dxtbx.datablock import DataBlockFactory
+from dxtbx.model.experiment_list import ExperimentListFactory
+from dials.algorithms.image.threshold import DispersionThresholdDebug
+from dials.array_family import flex
+import pickle
+
+# remember to run DUI and generate the needed file in dui_files
+# do import and then do mask tool
+
+n_json_file_path = "/tmp/dui_run/dui_files/2_datablock.json"
+datablocks = DataBlockFactory.from_json_file(n_json_file_path)
+# TODO check length of datablock for safety
+datablock = datablocks[0]
+my_sweep = datablock.extract_sweeps()[0]
+img_arr = my_sweep.get_raw_data(0)[0].as_double()
+img_raw = my_sweep.get_raw_data(0)[0]
+
+###############################################################################
+
+experiments = ExperimentListFactory.from_json_file(
+                n_json_file_path, check_format=False
+            )
+
+imageset_tmp = experiments.imagesets()[0]
+mask_file = imageset_tmp.external_lookup.mask.filename
+
+pick_file = open(mask_file, "rb")
+mask_tup_obj = pickle.load(pick_file)
+pick_file.close()
+
+mask_flex = mask_tup_obj[0]
+
+##############################################################################
+
+algorithm = DispersionThresholdStrategy(
+        kernel_size = (3, 3),
+        gain = 1,
+        n_sigma_b = 6,
+        n_sigma_s = 3,
+        min_count = 2,
+        global_threshold = 0
+)
+
+##############################################################################
+copy_pasted = '''
+DispersionThresholdDebug(
+                        raw_data[i_panel].as_double(),
+                        mask[i_panel],
+                        gain_map[i_panel],
+                        size,
+                        nsigma_b,
+                        nsigma_s,
+                        global_threshold,
+                        min_local,
+                    )
+'''
+
+gain_value = 1
+gain_map = [
+    flex.double(img_raw.accessor(), gain_value)
+]
+test_algo = DispersionThresholdDebug(
+                        img_arr,
+                        mask_flex,
+                        gain_map,
+                        (3, 3),
+                        6,
+                        3,
+                        0,
+                        2,
+                    )
+
+###############################################################################
+algr = algorithm(img_arr, mask_flex)
+
+np_alg = algr.as_numpy_array()
+
+print np_alg
+
+
+from matplotlib import pyplot as plt
+plt.imshow( np_alg , interpolation = "nearest" )
+plt.show()
+
