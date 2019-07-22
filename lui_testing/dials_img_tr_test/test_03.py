0112a227921eea773d2985aa07591d814a6edc28
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2019-07-22 14:59:45 +0100

    cut/trim code from example

--- lui_testing/dials_img_tr_test/test_03.py
+++ lui_testing/dials_img_tr_test/test_03.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import, division, print_function
 
 
 class Test:
-    def setup_class(self):
+    def __init__(self):
         from scitbx.array_family import flex
 
         # Create an image
@@ -15,87 +15,6 @@ class Test:
         self.size = (3, 3)
         self.min_count = 2
 
-    def test_niblack(self):
-        from dials.algorithms.image.threshold import niblack
-
-        n_sigma = 3
-        result = niblack(self.image, self.size, n_sigma)
-
-    def test_sauvola(self):
-        from dials.algorithms.image.threshold import sauvola
-
-        k = 3
-        r = 3
-        result = sauvola(self.image, self.size, k, r)
-
-    def test_index_of_dispersion(self):
-        from dials.algorithms.image.threshold import index_of_dispersion
-
-        n_sigma = 3
-        result = index_of_dispersion(self.image, self.size, n_sigma)
-
-    def test_index_of_dispersion_masked(self):
-        from dials.algorithms.image.threshold import index_of_dispersion_masked
-
-        n_sigma = 3
-        result = index_of_dispersion_masked(
-            self.image, self.mask, self.size, self.min_count, n_sigma
-        )
-
-    def test_gain(self):
-        from dials.algorithms.image.threshold import gain
-
-        n_sigma = 3
-        result = gain(
-            self.image, self.mask, self.gain, self.size, self.min_count, n_sigma
-        )
-
-    def test_dispersion(self):
-        from dials.algorithms.image.threshold import dispersion
-
-        nsig_b = 3
-        nsig_s = 3
-        result = dispersion(
-            self.image, self.mask, self.size, nsig_b, nsig_s, self.min_count
-        )
-
-    def test_dispersion_w_gain(self):
-        from dials.algorithms.image.threshold import dispersion_w_gain
-
-        nsig_b = 3
-        nsig_s = 3
-        result1 = dispersion_w_gain(
-            self.image, self.mask, self.gain, self.size, nsig_b, nsig_s, self.min_count
-        )
-
-        # scaling both the image and the gain should not affect the result
-        result2 = dispersion_w_gain(
-            2.0 * self.image,
-            self.mask,
-            2.0 * self.gain,
-            self.size,
-            nsig_b,
-            nsig_s,
-            self.min_count,
-        )
-        assert result1 == result2
-
-        # should get the same result as dispersion if the gain is unity
-        from dials.algorithms.image.threshold import dispersion
-
-        result3 = dispersion(
-            self.image, self.mask, self.size, nsig_b, nsig_s, self.min_count
-        )
-        result4 = dispersion_w_gain(
-            self.image,
-            self.mask,
-            (0 * self.gain + 1),
-            self.size,
-            nsig_b,
-            nsig_s,
-            self.min_count,
-        )
-        assert result3 == result4
 
     def test_dispersion_debug(self):
         from dials.algorithms.image.threshold import dispersion
@@ -129,28 +48,10 @@ class Test:
         result4 = debug.final_mask()
         assert result3 == result4
 
-    def test_dispersion_threshold(self):
-        from dials.algorithms.image.threshold import dispersion, dispersion_w_gain
-        from dials.algorithms.image.threshold import DispersionThreshold
-        from dials.array_family import flex
 
-        nsig_b = 3
-        nsig_s = 3
-        algorithm = DispersionThreshold(
-            self.image.all(), self.size, nsig_b, nsig_s, 0, self.min_count
-        )
 
-        result1 = dispersion(
-            self.image, self.mask, self.size, nsig_b, nsig_s, self.min_count
-        )
+if __name__ == "__main__":
+    print("Hi")
 
-        result2 = dispersion_w_gain(
-            self.image, self.mask, self.gain, self.size, nsig_b, nsig_s, self.min_count
-        )
-
-        result3 = flex.bool(flex.grid(self.image.all()))
-        result4 = flex.bool(flex.grid(self.image.all()))
-        algorithm(self.image, self.mask, result3)
-        algorithm(self.image, self.mask, self.gain, result4)
-        assert result1 == result3
-        assert result2 == result4
+    test1 = Test()
+    a = test1.test_dispersion_debug()
