3575a6cf08e46dbccbab1f8584579fb6053f6c6b
luisodls <luis.fuentes-montero@diamond.ac.uk>
2019-12-16 17:16:59 +0000

    playing with sockets in Py3 & PySySide2

---
+++ lui_testing/sockets/py3_server.py
@@ -0,0 +1,24 @@
+import socket
+
+n_connections = 15
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('localhost', 8089))
+#serversocket.bind((8089))
+serversocket.listen(n_connections) # become a server socket, maximum 15 connections
+buf = ""
+for i in range(n_connections):
+    connection, address = serversocket.accept()
+    buf = str(connection.recv(640))
+    if len(buf) > 0:
+        print("...>>> buf =", buf)
+        if( buf == "stop" ):
+            print("Stoping")
+            break
+
+    else:
+        print("len(buf) <= 0")
+
+    print("\n connection num ", i)
+
+
