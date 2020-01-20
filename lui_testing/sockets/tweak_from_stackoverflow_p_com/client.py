d2f8320042f39e3fbf86a7f12f418b2bc9f051a0
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2020-01-20 15:09:29 +0000

    adding new starting point example for client-server code testing

---
+++ lui_testing/sockets/tweak_from_stackoverflow_p_com/client.py
@@ -0,0 +1,56 @@
+
+'''
+tweaked version of code copied from:
+
+https://stackoverflow.com/questions/41167409/pyqt5-sending-and-receiving-messages-between-client-and-server
+'''
+
+from PySide2.QtCore import QDataStream, QIODevice
+from PySide2.QtWidgets import QApplication, QDialog
+from PySide2.QtNetwork import QTcpSocket, QAbstractSocket
+
+class Client(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.tcpSocket = QTcpSocket(self)
+        self.blockSize = 0
+        self.makeRequest()
+        self.tcpSocket.waitForConnected(1000)
+        self.tcpSocket.write(b'hello')
+        self.tcpSocket.readyRead.connect(self.dealCommunication)
+        self.tcpSocket.error.connect(self.displayError)
+
+    def makeRequest(self):
+        HOST = '127.0.0.1'
+        PORT = 8000
+        self.tcpSocket.connectToHost(HOST, PORT, QIODevice.ReadWrite)
+
+    def dealCommunication(self):
+        instr = QDataStream(self.tcpSocket)
+        instr.setVersion(QDataStream.Qt_5_0)
+        if self.blockSize == 0:
+            if self.tcpSocket.bytesAvailable() < 2:
+                return
+
+            self.blockSize = instr.readUInt16()
+
+        if self.tcpSocket.bytesAvailable() < self.blockSize:
+            return
+
+        print("Printing from client")
+        print(str(instr.readString()))
+        print("done ... Client")
+
+    def displayError(self, socketError):
+        if socketError == QAbstractSocket.RemoteHostClosedError:
+            pass
+        else:
+            print(self, "The following error occurred: %s." % self.tcpSocket.errorString())
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+    client = Client()
+    sys.exit(client.exec_())
---
+++ lui_testing/sockets/tweak_from_stackoverflow_p_com/server.py
@@ -0,0 +1,51 @@
+import sys
+from PySide2.QtCore import QByteArray, QDataStream, QIODevice
+from PySide2.QtWidgets import QApplication, QDialog
+from PySide2.QtNetwork import QHostAddress, QTcpServer
+
+class Server(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.tcpServer = None
+
+    def sessionOpened(self):
+        self.tcpServer = QTcpServer(self)
+        PORT = 8000
+        address = QHostAddress('127.0.0.1')
+        if not self.tcpServer.listen(address, PORT):
+            print("cant listen!")
+            self.close()
+            return
+
+        self.tcpServer.newConnection.connect(self.dealCommunication)
+
+    def dealCommunication(self):
+        clientConnection = self.tcpServer.nextPendingConnection()
+        block = QByteArray()
+        out = QDataStream(block, QIODevice.ReadWrite)
+        out.setVersion(QDataStream.Qt_5_0)
+        out.writeUInt16(0)
+
+        message = "Goodbye!"
+        message = str(message)
+
+        out.writeString(message)
+        out.device().seek(0)
+        out.writeUInt16(block.size() - 2)
+        clientConnection.waitForReadyRead()
+        instr = clientConnection.readAll()
+
+        print("Printing from server")
+        print(str(instr, encoding='ascii'))
+        print("done ... Server")
+
+        clientConnection.disconnected.connect(clientConnection.deleteLater)
+        clientConnection.write(block)
+        clientConnection.disconnectFromHost()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    server = Server()
+    server.sessionOpened()
+    sys.exit(server.exec_())
