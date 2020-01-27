eadbd1da729f554e57a8d0ba846b940083663c9f
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2020-01-27 11:03:16 +0000

    new test with new approach for client server app

---
+++ lui_testing/sockets/dual_dialog_test_02/client.py
@@ -0,0 +1,91 @@
+
+'''
+tweaked version of code copied from:
+
+https://stackoverflow.com/questions/41167409/pyqt5-sending-and-receiving-messages-between-client-and-server
+'''
+
+import sys
+from PySide2.QtNetwork import QTcpSocket, QAbstractSocket
+from PySide2.QtGui import QTextCursor
+
+from PySide2.QtCore import (
+    QDataStream,
+    QIODevice
+    )
+
+from PySide2.QtWidgets import (
+    QApplication,
+    QDialog,
+    QVBoxLayout,
+    QPushButton,
+    QLineEdit,
+    QTextEdit
+    )
+
+class Client(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.tcpSocket = QTcpSocket(self)
+        self.tcpSocket.readyRead.connect(self.dealCommunication)
+        self.tcpSocket.error.connect(self.displayError)
+
+        self.blockSize = 0
+
+        main_box = QVBoxLayout()
+        self.incoming_text = QTextEdit()
+        main_box.addWidget(self.incoming_text)
+        low_hbox = QVBoxLayout()
+        self.txt_in = QLineEdit('test text')
+        low_hbox.addWidget(self.txt_in)
+        send_but = QPushButton("send MSG")
+
+        send_but.clicked.connect(self.build_request)
+
+        low_hbox.addWidget(send_but)
+        main_box.addLayout(low_hbox)
+        self.setLayout(main_box)
+
+    def build_request(self):
+        txt2send = str.encode(self.txt_in.text())
+        self.makeRequest()
+        self.tcpSocket.write(txt2send)
+
+    def makeRequest(self):
+        HOST = '127.0.0.1'
+        PORT = 8000
+        self.tcpSocket.connectToHost(HOST, PORT, QIODevice.ReadWrite)
+        self.tcpSocket.waitForConnected(1000)
+
+    def dealCommunication(self):
+
+        self.tcpSocket.waitForConnected(1000) ###
+
+        instr = QDataStream(self.tcpSocket)
+        instr.setVersion(QDataStream.Qt_5_0)
+
+        self.blockSize = instr.readUInt16()
+
+        if self.tcpSocket.bytesAvailable() < self.blockSize:
+            print("self.tcpSocket.bytesAvailable() < self.blockSize")
+            return
+
+        self.incoming_text.moveCursor(QTextCursor.End)
+        read_str = instr.readString()
+        str_instr = str(read_str)
+        self.incoming_text.insertPlainText(str_instr + "\n")
+
+        print("from client: ", str_instr, "<<")
+
+    def displayError(self, socketError):
+        if socketError == QAbstractSocket.RemoteHostClosedError:
+            pass
+
+        else:
+            print(self, "The following error occurred: %s." % self.tcpSocket.errorString())
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    client = Client()
+    sys.exit(client.exec_())
---
+++ lui_testing/sockets/dual_dialog_test_02/server.py
@@ -0,0 +1,85 @@
+
+'''
+tweaked version of code copied from:
+
+https://stackoverflow.com/questions/41167409/pyqt5-sending-and-receiving-messages-between-client-and-server
+'''
+
+import sys
+from PySide2.QtCore import QByteArray, QDataStream, QIODevice
+from PySide2.QtNetwork import QHostAddress, QTcpServer
+
+from PySide2.QtGui import QTextCursor
+
+from PySide2.QtWidgets import (
+    QApplication,
+    QDialog,
+    QVBoxLayout,
+    QPushButton,
+    QLineEdit,
+    QTextEdit
+    )
+
+class Server(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.tcpServer = None
+
+        main_box = QVBoxLayout()
+        self.incoming_text = QTextEdit()
+        main_box.addWidget(self.incoming_text)
+        low_hbox = QVBoxLayout()
+        self.txt_in = QLineEdit('test text')
+        low_hbox.addWidget(self.txt_in)
+
+        send_but = QPushButton("send MSG")
+        send_but.clicked.connect(self.build_msg_back)
+        low_hbox.addWidget(send_but)
+
+        main_box.addLayout(low_hbox)
+        self.setLayout(main_box)
+
+    def sessionOpened(self):
+        self.tcpServer = QTcpServer(self)
+        self.MyPort = 8000
+        self.MyAddress = QHostAddress('127.0.0.1')
+        if not self.tcpServer.listen(self.MyAddress, self.MyPort):
+            print("cant listen!")
+            self.close()
+            return
+
+        self.tcpServer.newConnection.connect(self.dealCommunication)
+
+    def dealCommunication(self):
+        self.clientConnection = self.tcpServer.nextPendingConnection()
+        self.clientConnection.waitForReadyRead()
+        instr = self.clientConnection.readAll()
+
+        self.incoming_text.moveCursor(QTextCursor.End)
+        str_instr = str(instr, 'utf-8')
+        self.incoming_text.insertPlainText( str_instr + "\n" )
+        print("from server: ", str(str_instr), "<<")
+
+    def build_msg_back(self):
+
+        block = QByteArray()
+        out = QDataStream(block, QIODevice.ReadWrite)
+        out.setVersion(QDataStream.Qt_5_0)
+        out.writeUInt16(0)
+
+        message = str(self.txt_in.text())
+        print("message(server) = ", message)
+        out.writeString(message)
+        out.device().seek(0)
+        out.writeUInt16(block.size() - 2)
+        print("(server) out.status()", out.status())
+        self.clientConnection.disconnected.connect(self.clientConnection.deleteLater)
+        self.clientConnection.write(block)
+        self.clientConnection.disconnectFromHost()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    server = Server()
+    server.sessionOpened()
+    sys.exit(server.exec_())
