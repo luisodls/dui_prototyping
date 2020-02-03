2f886eb613d8693a87561d9c27a0eccf717e02d4
luisolafg <luicub@gmail.com>
2020-02-03 11:58:07 +0000

    adding example that is suppoced to to run on PyQt4 py2

---
+++ lui_testing/sockets/py2_example_02/client.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+from __future__ import division, print_function
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtNetwork import *
+
+class Client( QDialog):
+    def __init__(self, parent=None):
+        super(Client, self).__init__(parent)
+
+        self.incoming_text =  QTextEdit()
+        self.dataLineEdit =  QLineEdit('test text')
+        send2serverButton =  QPushButton("send to server")
+
+        self.tcpSocket =  QTcpSocket(self)
+
+        send2serverButton.clicked.connect(self.requestNewConnection)
+        self.tcpSocket.readyRead.connect(self.readFromServer)
+        self.tcpSocket.error.connect(self.displayError)
+
+        mainLayout =  QVBoxLayout()
+        mainLayout.addWidget(self.incoming_text)
+        mainLayout.addWidget( QLabel(" \n Type here"))
+        mainLayout.addWidget(self.dataLineEdit)
+        mainLayout.addWidget(send2serverButton)
+        self.setLayout(mainLayout)
+        self.setWindowTitle("Fortune Client")
+
+        self.tcpSocket.stateChanged.connect(self.tell_State)
+
+    def tell_State(self):
+        print("self.tcpSocket.state()", self.tcpSocket.state())
+        print("self.tcpSocket.isValid()", self.tcpSocket.isValid())
+
+    def requestNewConnection(self):
+        if not self.tcpSocket.isValid():
+            self.tcpSocket.abort()
+            self.tcpSocket.connectToHost( QHostAddress.Any, 12354,  QIODevice.ReadWrite)
+
+        if self.tcpSocket.waitForConnected(1000):
+            print("Connected!")
+
+        else:
+            print("Failed to connect")
+
+        txt2send = str(self.dataLineEdit.text())
+        #txt2send = str.encode(self.dataLineEdit.text())
+        self.tcpSocket.write(txt2send)
+
+    def readFromServer(self):
+        print("client.readFromServer")
+        InStr =  QDataStream(self.tcpSocket)
+        #InStr.setVersion( QDataStream.Qt_5_0)
+
+        blockSize = InStr.readUInt16()
+
+        if self.tcpSocket.bytesAvailable() < blockSize:
+            print("tcpSocket.bytesAvailable() < blockSize")
+            return
+
+        nxt_str = InStr.readString()
+        print("nxt_str(client) =", nxt_str)
+        self.incoming_text.moveCursor(QtGui.QTextCursor.End)
+        self.incoming_text.insertPlainText(nxt_str + "\n")
+
+    def displayError(self, socketError):
+        if socketError ==  QAbstractSocket.RemoteHostClosedError:
+            pass
+        elif socketError ==  QAbstractSocket.HostNotFoundError:
+             QMessageBox.information(self, " Client",
+                    "The host was not found. Please check the host name and "
+                    "port settings.")
+        elif socketError ==  QAbstractSocket.ConnectionRefusedError:
+             QMessageBox.information(self, " Client",
+                    "The connection was refused by the peer. Make sure "
+                    "the server is running, and check that the host name "
+                    "and port settings are correct.")
+        else:
+             QMessageBox.information(self, " Client",
+                    "The following error occurred: %s." % self.tcpSocket.errorString())
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    app =  QApplication(sys.argv)
+    client = Client()
+    client.show()
+    sys.exit(client.exec_())
---
+++ lui_testing/sockets/py2_example_02/server.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+from __future__ import division, print_function
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from PyQt4.QtNetwork import *
+
+class Server( QDialog):
+    def __init__(self, parent=None):
+        super(Server, self).__init__(parent)
+
+        statusLabel =  QLabel()
+
+        self.tcpServer =  QTcpServer(self)
+        if not self.tcpServer.listen(address = QHostAddress.Any, port = 12354):
+            QMessageBox.critical(self, "Counter Server",
+                    "Unable to start the server: %s." % self.tcpServer.errorString())
+            self.close()
+            return
+
+        print("self.tcpServer.serverAddress()", self.tcpServer.serverAddress())
+
+        statusLabel.setText("The server is running on port %d.\nRun the "
+                "Counter Client example now." % self.tcpServer.serverPort())
+
+        self.counting = 0
+
+        self.tcpServer.newConnection.connect(self.new_connection)
+        mainLayout =  QVBoxLayout()
+        mainLayout.addWidget(statusLabel)
+
+        send_count_butt =  QPushButton("send counting")
+        send_count_butt.clicked.connect(self.send_counting)
+        mainLayout.addWidget(send_count_butt)
+
+        self.setLayout(mainLayout)
+        self.setWindowTitle("Counter Server")
+
+    def new_connection(self):
+        self.new_socket = self.tcpServer.nextPendingConnection()
+        self.new_socket.waitForReadyRead()
+        self.print_resived_str()
+        self.new_socket.channelReadyRead.connect(self.channel_ready_read)
+
+    def channel_ready_read(self):
+        print("channel_ready_read(server)")
+        self.print_resived_str()
+
+    def print_resived_str(self):
+        instr = self.new_socket.readAll()
+
+        str_instr = str(instr)
+        print("Printing from server")
+        print("<<", str(str_instr), ">>")
+        print("done ... Server")
+
+
+    def send_counting(self):
+        block =  QByteArray()
+        out =  QDataStream(block,  QIODevice.ReadWrite)
+        #out.setVersion( QDataStream.Qt_5_0)
+        out.writeUInt16(0)
+        self.counting += 1
+        counter_str = "number of clicks =" + str(self.counting) + " so far"
+        out.writeString(counter_str)
+        out.device().seek(0)
+        out.writeUInt16(block.size() - 2)
+
+        self.new_socket.write(block)
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    app =  QApplication(sys.argv)
+    server = Server()
+    sys.exit(server.exec_())
