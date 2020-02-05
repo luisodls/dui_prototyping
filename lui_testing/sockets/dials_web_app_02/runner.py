23201d3fb04fc86798258c97e8a3ed82c96a1c65
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2020-02-05 15:11:30 +0000

    building dials web app

---
+++ lui_testing/sockets/dials_web_app_02/runner.py
@@ -0,0 +1,59 @@
+import subprocess
+
+from PySide2 import QtCore, QtWidgets, QtGui, QtNetwork
+
+class CommandThread(QtCore.QThread):
+
+    str_print_signal = QtCore.Signal(str)
+    str_fail_signal = QtCore.Signal()
+
+    def __init__(self, parent=None):
+        super(CommandThread, self).__init__()
+
+    def __call__(self, cmd_to_run, ref_to_controler):
+        self.cmd_to_run = cmd_to_run
+        self.ref_to_controler = ref_to_controler
+        self.status_thread = CheckStatusThread()
+        self.status_thread(self.ref_to_controler)
+        self.start()
+
+    def run(self):
+        print("self.cmd_to_run =", self.cmd_to_run)
+        self.ref_to_controler.run(command=self.cmd_to_run, ref_to_class=self)
+
+    def emit_print_signal(self, str_lin):
+        print(str_lin, ":<<")
+        #self.str_print_signal.emit(str_lin)
+
+    def emit_fail_signal(self):
+        #self.str_fail_signal.emit()
+        print("emit_fail_signal")
+
+
+def run_cmd(cmd2run):
+    cmd_thread = CommandThread()
+    print("\nRunning:", cmd2run, "\n")
+
+    my_process = subprocess.Popen(
+        cmd2run,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT,
+        bufsize=1,
+    )
+
+    for line in iter(my_process.stdout.readline, b""):
+        single_line = line[0 : len(line) - 1]
+        print(">>: ", single_line)
+        cmd_thread.emit_print_signal(str_lin = single_line)
+
+    my_process.wait()
+    my_process.stdout.close()
+
+    print(cmd2run, "finished")
+
+
+if __name__ == "__main__":
+    run_cmd("ls -al ../")
+    run_cmd("dials.import ")
+
---
+++ lui_testing/sockets/dials_web_app_02/server.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+from PySide2 import QtCore, QtWidgets, QtNetwork
+
+class Server(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super(Server, self).__init__(parent)
+
+        statusLabel = QtWidgets.QLabel()
+
+        self.tcpServer = QtNetwork.QTcpServer(self)
+        if not self.tcpServer.listen(address =QtNetwork.QHostAddress.Any, port = 12354):
+            QtWidgets.QMessageBox.critical(self, "Counter Server",
+                    "Unable to start the server: %s." % self.tcpServer.errorString())
+            self.close()
+            return
+
+        print("self.tcpServer.serverAddress()", self.tcpServer.serverAddress())
+
+        statusLabel.setText("The server is running on port %d.\nRun the "
+                "Counter Client example now." % self.tcpServer.serverPort())
+
+        self.counting = 0
+
+        self.tcpServer.newConnection.connect(self.new_connection)
+        mainLayout = QtWidgets.QVBoxLayout()
+        mainLayout.addWidget(statusLabel)
+
+        send_count_butt = QtWidgets.QPushButton("send counting")
+        send_count_butt.clicked.connect(self.send_counting)
+        mainLayout.addWidget(send_count_butt)
+
+        self.setLayout(mainLayout)
+        self.setWindowTitle("Counter Server")
+
+    def new_connection(self):
+        self.new_socket = self.tcpServer.nextPendingConnection()
+        self.new_socket.waitForReadyRead()
+        self.print_resived_str()
+        self.new_socket.channelReadyRead.connect(self.channel_ready_read)
+
+    def channel_ready_read(self):
+        print("channel_ready_read(server)")
+        self.print_resived_str()
+
+    def print_resived_str(self):
+        instr = self.new_socket.readAll()
+
+        #str_instr = str(instr, 'utf-8')
+        str_instr = str(instr.data().decode('utf-8'))
+
+        print("Printing from server")
+        print("<<", str(str_instr), ">>")
+        print("done ... Server")
+
+
+    def send_counting(self):
+        block = QtCore.QByteArray()
+        out = QtCore.QDataStream(block, QtCore.QIODevice.ReadWrite)
+        #out.setVersion(QtCore.QDataStream.Qt_5_0)
+        out.writeUInt16(0)
+        self.counting += 1
+        counter_str = "number of clicks =" + str(self.counting) + " so far"
+        out.writeString(counter_str)
+        out.device().seek(0)
+        out.writeUInt16(block.size() - 2)
+
+        self.new_socket.write(block)
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    app = QtWidgets.QApplication(sys.argv)
+    server = Server()
+    sys.exit(server.exec_())
