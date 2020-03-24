79d78ea86f7d5e9e88f414d35533f549a07528da
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-03-24 14:19:56 +0000

    adding to new guide script

--- lui_testing/prepare_chain.sh
+++ lui_testing/prepare_chain.sh
@@ -9,5 +9,9 @@ cd tst_chain/
 mkdir imp_dir
 cd imp_dir/
 dials.import /scratch/dui_data/X4_wide/X4_wide_M1S4_2_000*.cbf
+dials.import /scratch/dui_tst/X4_wide/X4_wide_M1S4_2_000*.cbf
+
 cd ..
 
+
+dials.python /scratch/dui_prototyping/lui_testing/py3_pyside2_n_dui2/subprocess_n_connections/chain_tst_02.py
