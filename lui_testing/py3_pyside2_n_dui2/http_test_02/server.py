3259d7e7a50e2b39960f8e6f83475cbbb9f48068
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-04-09 16:53:27 +0100

    adding main

--- lui_testing/py3_pyside2_n_dui2/http_test_02/server.py
+++ lui_testing/py3_pyside2_n_dui2/http_test_02/server.py
@@ -2,8 +2,9 @@ import http.server
 import socketserver
 
 PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as http_daemon:
-    print("serving at port", PORT)
-    http_daemon.serve_forever()
+if __name__ == "__main__":
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", PORT), Handler) as http_daemon:
+        print("serving at port", PORT)
+        http_daemon.serve_forever()
