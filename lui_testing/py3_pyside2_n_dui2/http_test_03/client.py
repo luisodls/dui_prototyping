a9cf6a114e7674cddf58e9b5f99b05904f786f1f
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-04-13 11:33:16 +0100

    testing if time.sleep is needed, looks like not

--- lui_testing/py3_pyside2_n_dui2/http_test_03/client.py
+++ lui_testing/py3_pyside2_n_dui2/http_test_03/client.py
@@ -1,14 +1,12 @@
-
 import requests
-import time
-
+#import time
 
 r = requests.get('http://localhost:8080/', stream=True)
 
 line_str = ''
 
 while True:
-    time.sleep(0.01)
+    #time.sleep(0.1)
     tmp_dat = r.raw.read(1)
     single_char = str(tmp_dat.decode('utf-8'))
     line_str += single_char
@@ -18,4 +16,7 @@ while True:
 
     elif line_str[-7:] == '/*EOF*/':
         print('/*EOF*/ received')
+        break
+
+    print('looping')
 
