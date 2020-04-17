b9e9b65412a689582783b7e12e034fad7ca82385
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-04-17 11:00:07 +0100

    one more step, still not in real time output

--- lui_testing/py3_pyside2_n_dui2/http_n_pyside_tst/client.py
+++ lui_testing/py3_pyside2_n_dui2/http_n_pyside_tst/client.py
@@ -3,19 +3,21 @@ import sys, time
 import requests
 
 class Client(QtWidgets.QDialog):
+    line_out = QtCore.Signal(str)
     def __init__(self, parent=None):
         super(Client, self).__init__(parent)
 
         self.incoming_text = QtWidgets.QTextEdit()
         self.incoming_text.setFont(QtGui.QFont("Monospace"))
+
         self.dataLineEdit = QtWidgets.QLineEdit()
-
         self.dataLineEdit.setPlaceholderText("Type command")
-
         self.dataLineEdit.setFont(QtGui.QFont("Monospace"))
 
         send2serverButton = QtWidgets.QPushButton("Launch command")
         send2serverButton.clicked.connect(self.request_launch)
+
+        self.line_out.connect(self.add_line)
 
         mainLayout = QtWidgets.QVBoxLayout()
         mainLayout.addWidget(self.incoming_text)
@@ -25,11 +27,13 @@ class Client(QtWidgets.QDialog):
         self.setLayout(mainLayout)
         self.setWindowTitle("DUI front end test")
 
+    def add_line(self, new_line):
+        self.incoming_text.moveCursor(QtGui.QTextCursor.End)
+        self.incoming_text.insertPlainText(new_line)
+
     def request_launch(self):
 
         cmd_str = str.encode(self.dataLineEdit.text())
-
-        print('entered:', cmd_str)
         cmd = {'command': [cmd_str]}
         r_g = requests.get('http://localhost:8080/', stream = True, params = cmd)
 
@@ -40,8 +44,7 @@ class Client(QtWidgets.QDialog):
             line_str += single_char
             if single_char == '\n':
                 print(line_str[:-1])
-                self.incoming_text.moveCursor(QtGui.QTextCursor.End)
-                self.incoming_text.insertPlainText(line_str)
+                self.line_out.emit(line_str)
                 line_str = ''
 
             elif line_str[-7:] == '/*EOF*/':
