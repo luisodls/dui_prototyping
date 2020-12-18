c3f914c82997b0c0c87932c776bb5b7fb81afd12
luisodls <luis.fuentes-montero@diamond.ac.uk>
2020-12-18 20:08:25 +0000

    starting new example, attempt to fix the issue with utf-8

---
+++ lui_testing/py3_pyside2_n_dui2/http_test_09/main_client_gui.py
@@ -0,0 +1,46 @@
+import sys
+import requests
+
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+from PySide2.QtWebEngineWidgets import QWebEngineView
+from PySide2 import QtUiTools
+
+class MainObject(QObject):
+    def __init__(self, parent = None):
+        super(MainObject, self).__init__(parent)
+        self.window = QtUiTools.QUiLoader().load("viewer.ui")
+        self.webEngineView = QWebEngineView()
+        self.loadPage()
+
+        self.window.horizontalMainLayout.addWidget(self.webEngineView)
+        self.window.show()
+
+    def loadPage(self):
+        r_g = requests.get(
+            'http://localhost:8182/', stream = True, params = "a"
+        )
+        full_file = ''
+        line_str = ''
+        while True:
+            tmp_dat = r_g.raw.read(2)
+            single_char = str(tmp_dat.decode('utf-16'))
+            line_str += single_char
+            if single_char == '\n':
+                full_file += line_str
+                line_str = ''
+
+            elif line_str[-7:] == '/*EOF*/':
+                print('/*EOF*/ received')
+                break
+
+        print("html:", full_file)
+        print("type(full_file):", type(full_file))
+        self.webEngineView.setHtml(full_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MainObject()
+    sys.exit(app.exec_())
---
+++ lui_testing/py3_pyside2_n_dui2/http_test_09/server.py
@@ -0,0 +1,29 @@
+import http.server
+import socketserver
+
+class ReqHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        f = open("report2.html", "r")
+        str_lst = f.readlines()
+        f.close()
+
+        for lin in str_lst:
+            self.wfile.write(bytes(lin, 'utf-16'))
+
+        self.wfile.write(bytes("/*EOF*/", 'utf-16'))
+
+
+if __name__ == "__main__":
+    PORT = 8182
+    with socketserver.TCPServer(("", PORT), ReqHandler) as http_daemon:
+        print("serving at port", PORT)
+        try:
+            http_daemon.serve_forever()
+
+        except KeyboardInterrupt:
+            http_daemon.server_close()
+
