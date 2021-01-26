edcb6cbe3693d5c572e1284bfec8c223890eb424
luisodls <luis.fuentes-montero@diamond.ac.uk>
2021-01-26 14:14:30 +0000

    measuring html load stages

--- lui_testing/py3_pyside2_n_dui2/http_test_10/client_gui_w_progress.py
+++ lui_testing/py3_pyside2_n_dui2/http_test_10/client_gui_w_progress.py
@@ -1,5 +1,4 @@
 import sys
-import requests
 
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
@@ -7,13 +6,17 @@ from PySide2.QtGui import *
 from PySide2.QtWebEngineWidgets import QWebEngineView
 from PySide2 import QtUiTools
 
-
 class MainObject(QObject):
     def __init__(self, parent = None):
         super(MainObject, self).__init__(parent)
 
         self.window = QtUiTools.QUiLoader().load("viewer.ui")
         self.webEngineView = QWebEngineView()
+
+        self.webEngineView.loadStarted.connect(self.load_started)
+        self.webEngineView.loadProgress.connect(self.load_progress)
+        self.webEngineView.loadFinished.connect(self.load_finished)
+
         self.loadPage()
 
         self.window.horizontalMainLayout.addWidget(self.webEngineView)
@@ -27,9 +30,17 @@ class MainObject(QObject):
             print("type(html) =", type(html))
             self.webEngineView.setHtml(html)
 
+    def load_started(self):
+        print("load_started")
+
+    def load_progress(self, progress):
+        print("load_progress:", progress)
+
+    def load_finished(self):
+        print("load_finished")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = MainObject()
     sys.exit(app.exec_())
-
