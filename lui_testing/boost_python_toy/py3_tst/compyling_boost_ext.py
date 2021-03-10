abf8de386d6ca54338d517b13a1a8e8c6b47b15e
luisodls <luis.fuentes-montero@diamond.ac.uk>
2021-03-10 15:15:55 +0000

    moving some spaces in compile script

--- lui_testing/boost_python_toy/py3_tst/compyling_boost_ext.py
+++ lui_testing/boost_python_toy/py3_tst/compyling_boost_ext.py
@@ -8,7 +8,9 @@ for pos, single_shar in enumerate(inc_path):
     if(single_shar == "/" ):
         cut_inc_path = inc_path[0:pos]
 
-com_lin_01 = "g++ -I" + inc_path + " -I" + cut_inc_path +  " -fPIC -c " + obj_name + ".cpp"
+com_lin_01 = "g++ -I" + inc_path + " -I" + \
+    cut_inc_path +  " -fPIC -c " + obj_name + ".cpp"
+
 print("com_lin_01 =\n", com_lin_01)
 
 lib_path = sysconfig.get_python_lib()
@@ -21,16 +23,17 @@ for pos, single_shar in enumerate(cut_lib_path):
     if(single_shar == "/" ):
         cut_cut_lib_path = cut_lib_path[0:pos]
 
-com_lin_02 = "g++ -shared " + obj_name + ".o -L" + cut_cut_lib_path + " -lboost_python38 -L" + \
-             cut_lib_path + "/config -lpython3.8 -o " + obj_name + ".so"
+com_lin_02 = "g++ -shared " + obj_name + ".o -L" +   \
+    cut_cut_lib_path + " -lboost_python38 -L" +      \
+    cut_lib_path + "/config -lpython3.8 -o " + obj_name + ".so"
 
 print("\n Compiling line 1:")
-print("cmd =", com_lin_01)
+print("cmd =", com_lin_01, "\n")
 err_msg_01 = shell_call(com_lin_01, shell=True)
 print("\n Compiling line 2:")
-print("cmd =", com_lin_02)
+print("cmd =", com_lin_02, "\n")
 err_msg_02 = shell_call(com_lin_02, shell=True)
-print("\n done compiling")
+print("\n Done compiling")
 
 if(err_msg_01 != 0 or err_msg_02 !=0 ):
     print("Failed to compile some C++ extensions ")
