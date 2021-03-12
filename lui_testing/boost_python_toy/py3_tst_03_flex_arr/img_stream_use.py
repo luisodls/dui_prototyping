a53d83ee8b22257ef906468cb646abbf5bbfbe3b
luisodls <luis.fuentes-montero@diamond.ac.uk>
2021-03-12 15:10:18 +0000

    removing no longer needed tests

--- lui_testing/boost_python_toy/py3_tst_03_flex_arr/img_stream_use.py
+++ lui_testing/boost_python_toy/py3_tst_03_flex_arr/img_stream_use.py
@@ -4,22 +4,6 @@ from dials.array_family import flex
 
 print(img_stream_ext.greet())
 
-lst = [4,7,0]
-a = img_stream_ext.lst_bunch(lst)
-print("a =", a)
-
-add_str = a[2] + a[3]
-print("add_str =", add_str)
-
-b = a[2].split(" ")
-print("b =", b)
-
-c = a[3].split(" ")
-print("c =", c)
-
-d = b + c
-print("d =", d)
-
 x_max = 8
 y_max = 9
 data_xyz_flex = flex.double(flex.grid(x_max, y_max), 0)
@@ -29,3 +13,4 @@ for x in range(x_max):
         data_xyz_flex[x, y] = float(i)
 
 print("data_xyz_flex = \n", data_xyz_flex.as_numpy_array())
+
