0afa841bd209decf0f23d994faf7bfc145996524
luisodls <luis.fuentes-montero@diamond.ac.uk>
2021-03-13 14:56:20 +0000

    strings and c++ flex arrays, now using boost

--- lui_testing/boost_python_toy/py3_tst_04_flex_arr/img_stream_ext.cpp
+++ lui_testing/boost_python_toy/py3_tst_04_flex_arr/img_stream_ext.cpp
@@ -1,16 +1,10 @@
 #include <boost/python.hpp>
 #include <iostream>
 #include <string>
-#include <cmath>
-/*
-#include <scitbx/array_family/boost_python/utils.h>
-#include <scitbx/array_family/boost_python/flex_fwd.h>
-#include <scitbx/array_family/shared.h>
-*/
 #include <scitbx/array_family/flex_types.h>
+#include <boost/lexical_cast.hpp>
 
 namespace py = boost::python;
-
 using scitbx::af::flex_double;
 using scitbx::af::flex_grid;
 using scitbx::af::flex_int;
@@ -24,17 +18,20 @@ py::list lst_bunch(flex_double& data2d)
 {
     py::list data_out;
     std::string std_str = "";
-    int col, row;
     int nrow = data2d.accessor().all()[0];
     int ncol = data2d.accessor().all()[1];
+    int col, row;
+
     std::cout << "nrow = " << nrow << "\n";
     std::cout << "ncol = " << ncol << "\n";
-        for (row = 0; row < nrow; row++) {
-            for (col = 0; col < ncol; col++) {
-                std_str.append(std::to_string(data2d(row, col)));
-                std_str.append(",");
-            }
+    for (row = 0; row < nrow; row++) {
+        for (col = 0; col < ncol; col++) {
+            std_str.append(
+                boost::lexical_cast<std::string>(data2d(row, col))
+            );
+            std_str.append(",");
         }
+    }
     data_out.append(std_str);
     /*
      * This example shows how play with strings and lists
