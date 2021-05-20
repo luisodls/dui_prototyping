9e1c18e7f10221c57850897ec1ee106ab8bd40d9
luisodls <luis.fuentes-montero@diamond.ac.uk>
2021-05-20 16:12:55 +0100

    organizing example code

--- lui_testing/imgs_n_numpy_n_sockets/np_n_files.py
+++ lui_testing/imgs_n_numpy_n_sockets/np_n_files.py
@@ -18,18 +18,26 @@ def draw_pyplot(img_arr):
     plt.imshow(img_arr, interpolation = "nearest")
     plt.show()
 
+def save_np_array(np_array_in):
+    np.save(
+        "arr_img.npy", np_array_in,
+        allow_pickle = False, fix_imports = False
+    )
+
+def load_np_array():
+    img_arr_load = np.load(
+        "arr_img.npy", mmap_mode = None, allow_pickle = False,
+        fix_imports = False, encoding = 'ASCII'
+    )
+    return img_arr_load
+
+
 if __name__ == "__main__":
     ncol = 50
     nrow = 30
     img_arr = build_img_arr(nrow, ncol)
 
-    np.save(
-        "arr_img.npy", img_arr, allow_pickle = False, fix_imports = False
-    )
+    save_np_array(img_arr)
+    loaded_array = load_np_array()
 
-    img_arr_load = np.load(
-        "arr_img.npy", mmap_mode = None, allow_pickle = False,
-        fix_imports = False, encoding = 'ASCII'
-    )
-
-    draw_pyplot(img_arr_load)
+    draw_pyplot(loaded_array)
