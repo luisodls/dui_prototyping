2a1983ec25a3f78352a0c46cca37fb456ec3e4ee
luisodls <luis.fuentes-montero@diamond.ac.uk>
2021-05-21 20:31:57 +0100

    some time measuring

--- lui_testing/imgs_n_numpy_n_sockets/py3_tst_04_flex_arr/img_stream_use.py
+++ lui_testing/imgs_n_numpy_n_sockets/py3_tst_04_flex_arr/img_stream_use.py
@@ -1,16 +1,25 @@
 import img_stream_ext
-import numpy as np
+#import numpy as np
 from dials.array_family import flex
+from matplotlib import pyplot as plt
 
-x_max = 24
-y_max = 16
-data_xyz_flex = flex.double(flex.grid(x_max, y_max), 0)
-for x in range(x_max):
-    for y in range(y_max):
-        i = (x + y) / 2
-        data_xyz_flex[x, y] = float(i)
 
-#print("data_xyz_flex = \n", data_xyz_flex.as_numpy_array())
+def draw_pyplot(img_arr):
+    plt.imshow(img_arr, interpolation = "nearest")
+    plt.show()
 
-tst = img_stream_ext.img_arr_2_str(data_xyz_flex)
-print("tst =", tst)
+if __name__ == "__main__":
+    x_max = 2400
+    y_max = 1600
+    data_xyz_flex = flex.double(flex.grid(x_max, y_max), 0)
+    for x in range(x_max):
+        for y in range(y_max):
+            i = (x + y) / 2
+            data_xyz_flex[x, y] = float(i)
+
+    draw_pyplot(data_xyz_flex.as_numpy_array())
+
+    print("before img_arr_2_str  ...")
+    tst_str = img_stream_ext.img_arr_2_str(data_xyz_flex)
+    print("...  after img_arr_2_str")
+    print("tst_str =", tst_str)
