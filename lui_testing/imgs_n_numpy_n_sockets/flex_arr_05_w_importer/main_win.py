338793b2877d54c365b43552c44451af23966174
luisolafg <luicub@gmail.com>
2021-05-28 12:10:00 +0100

    starting to work on monocrome palette

--- lui_testing/imgs_n_numpy_n_sockets/flex_arr_05_w_importer/main_win.py
+++ lui_testing/imgs_n_numpy_n_sockets/flex_arr_05_w_importer/main_win.py
@@ -100,6 +100,63 @@ class np2bmp_heat(object):
         return img_array
 
 
+class np2bmp_monocrome(object):
+    def __init__(self):
+        self.red_byte = np.empty( (255), 'int')
+        for i in range(255):
+            self.red_byte[i] = i
+
+    def img_2d_rgb(
+        self, data2d = None, invert = False, i_min_max = [None, None]
+    ):
+        data2d_ini = np.copy(data2d)
+        if(i_min_max == [None, None]):
+            i_min_max = [data2d_ini.min(), data2d_ini.max()]
+            print("no max and min provided, assuming:", i_min_max)
+
+        elif(i_min_max[0] > data2d_ini.min() or i_min_max[1] < data2d_ini.max()):
+            print("clipping to [max, min]:", i_min_max, "  ...")
+            np.clip(data2d_ini, i_min_max[0], i_min_max[1], out = data2d_ini)
+            print("... done clipping")
+
+        self.width = np.size( data2d_ini[0:1, :] )
+        self.height = np.size( data2d_ini[:, 0:1] )
+
+        data2d_pos = data2d_ini[:,:] - i_min_max[0] + 1.0
+        data2d_pos_max = data2d_pos.max()
+
+        calc_pos_max = i_min_max[1] - i_min_max[0] + 1.0
+        if(calc_pos_max > data2d_pos_max):
+            data2d_pos_max = calc_pos_max
+
+        div_scale = 254.0 / data2d_pos_max
+        data2d_scale = np.multiply(data2d_pos, div_scale)
+        if(invert == True):
+            for x in np.nditer(
+                data2d_scale[:,:], op_flags=['readwrite'],
+                flags=['external_loop']
+            ):
+                x[...] = 254.0 - x[...]
+
+        img_array = np.zeros([self.height, self.width, 4], dtype=np.uint8)
+        img_array_r = np.empty( (self.height, self.width), 'int')
+        scaled_i = np.empty( (self.height, self.width), 'int')
+        scaled_i[:,:] = data2d_scale[:,:]
+
+        img_array_r[:,:] = scaled_i[:,:]
+        for x in np.nditer(
+            img_array_r[:,:], op_flags=['readwrite'],
+            flags=['external_loop']
+        ):
+            x[...] = self.red_byte[x]
+
+        img_array[:, :, 3] = 255
+        img_array[:, :, 2] = img_array_r[:,:] #Blue
+        img_array[:, :, 1] = img_array_r[:,:] #Green
+        img_array[:, :, 0] = img_array_r[:,:] #Red
+        return img_array
+
+
 def load_json_w_str():
     with open("arr_img.json") as json_file:
         arr_dic = json.load(json_file)
@@ -120,6 +177,7 @@ class Form(QObject):
         self.window = QtUiTools.QUiLoader().load("my_win.ui")
         self.my_scene_1 = QGraphicsScene()
         self.bmp_heat = np2bmp_heat()
+        self.bmp_m_cro = np2bmp_monocrome()
         self.window.graphicsView.setScene(self.my_scene_1)
         self.window.graphicsView.setDragMode(QGraphicsView.ScrollHandDrag)
         self.window.LoadButton.clicked.connect(self.btn_clk)
@@ -127,8 +185,8 @@ class Form(QObject):
 
     def btn_clk(self):
         np_array_img = load_json_w_str()
-        rgb_np = self.bmp_heat.img_2d_rgb(
-            data2d = np_array_img, invert = False, i_min_max = [-2, 50]
+        rgb_np = self.bmp_m_cro.img_2d_rgb(
+            data2d = np_array_img, invert = True, i_min_max = [-2, 50]
         )
         q_img = QImage(
             rgb_np.data,
