3676d2a0664530da8d325e7f041e643764148c4a
luisodls <luis.fuentes-montero@diamond.ac.uk>
2021-06-03 11:48:17 +0100

    playing with streams in C++

--- lui_testing/imgs_n_numpy_n_sockets/flex_arr_06_w_requests/img_stream_ext.cpp
+++ lui_testing/imgs_n_numpy_n_sockets/flex_arr_06_w_requests/img_stream_ext.cpp
@@ -16,25 +16,32 @@ std::string img_arr_2_str(flex_double& data2d)
     int d2 = data2d.accessor().all()[1];
     int i, j;
 
-    std::stringstream stream_data;
-    stream_data << std::fixed;
-    stream_data << std::setprecision(2);
-    stream_data.str("");
-    stream_data.clear();
-    stream_data << "{\n  \"d1\": " << d1 << ",\n  \"d2\": " << d2 << ",\n";
-    stream_data << "  \"str_data\": \"";
+    std::stringstream full_stream;
+    full_stream << std::fixed;
+    full_stream.str("");
+    full_stream.clear();
+
+    std::stringstream stream_array;
+    stream_array << std::fixed;
+    stream_array << std::setprecision(2);
+    stream_array.str("");
+    stream_array.clear();
+
+    full_stream << "{ " << std::quoted("d1") << ": " << d1;
+    full_stream << ", " << std::quoted("d2") << ": " << d2;
+    full_stream << ", " << std::quoted("str_data") << ": ";
 
     std::cout << "looping thru an array of: (" << d1 << ", " << d2 << ") ...\n";
     for (i = 0; i < d1; i++) {
         for (j = 0; j < d2; j++) {
-            stream_data << data2d(i, j);
+            stream_array << data2d(i, j);
             if(i < d1 - 1 or j < d2 - 1){
-                stream_data << ",";
+                stream_array << ",";
             };
         }
     }
-    stream_data << "\"\n}";
-    std_str = stream_data.str();
+    full_stream << std::quoted(stream_array.str()) << " }";
+    std_str = full_stream.str();
     std::cout << "... loop ended\n";
     return std_str;
 }
