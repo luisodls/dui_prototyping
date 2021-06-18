fa0bd2e4af8a9bb220b6f0d2a8f3bca4a8899a47
luisolafg <luicub@gmail.com>
2021-06-18 18:41:39 +0100

    working on reflection list as request

--- lui_testing/py3_pyside2_n_dui2/imgs_n_numpy_n_sockets/img_transf_01_reflel_lst/refl_data_request.py
+++ lui_testing/py3_pyside2_n_dui2/imgs_n_numpy_n_sockets/img_transf_01_reflel_lst/refl_data_request.py
@@ -1,90 +1,68 @@
-from dials.array_family import flex
 import json
-#import time
-#import zlib
-from dxtbx.model.experiment_list import ExperimentListFactory
+import requests
 
-def list_p_arrange(pos_col, hkl_col, pan_col, n_imgs):
-    img_lst = []
-    for time in range(n_imgs):
-        img_lst.append([])
+def json_data_request(url, cmd):
+    '''
+    this is a carbon copy of the function in upstream DUI2
+    '''
+    try:
+        req_get = requests.get(url, stream = True, params = cmd, timeout = 3)
+        str_lst = ''
+        line_str = ''
 
-    txt_lab = "updating Predicted Reflections Data:"
-    #my_bar = ProgBarBox(min_val=0, max_val=len(pos_col), text=txt_lab)
-    print(" len(pos_col) = ", len(pos_col))
-
-    for i, pos_tri in enumerate(pos_col):
-        #my_bar(i)
-
-        x_ini = '{:.2f}'.format(pos_tri[0] - 1)
-        y_ini = '{:.2f}'.format((pos_tri[1] - 1) + pan_col[i] * 213)
-
-        #x_ini = pos_tri[0] - 1
-        #y_ini = (pos_tri[1] - 1) + pan_col[i] * 213
-
-        if len(hkl_col) <= 1:
-            local_hkl = ""
-
-        else:
-            local_hkl = hkl_col[i]
-            if local_hkl == "(0, 0, 0)":
-                local_hkl = "NOT indexed"
-
-        xrs_size = 1
-        int_z_centr = int(pos_tri[2])
-        max_xrs_siz = 3
-        for idx in range(int_z_centr - max_xrs_siz, int_z_centr + max_xrs_siz):
-            xrs_size = max_xrs_siz - abs(int_z_centr - idx)
-            if idx == int_z_centr:
-                size2 = 2
+        #while True:
+        times_loop = 10
+        json_out = ""
+        for count_times in range(times_loop):
+            tmp_dat = req_get.raw.readline()
+            line_str = str(tmp_dat.decode('utf-8'))
+            if line_str[-7:] == '/*EOF*/':
+                print('/*EOF*/ received')
+                break
 
             else:
-                size2 = 0
+                str_lst = line_str
 
-            dat_to_append = [
-                x_ini + "," + y_ini + "," +
-                str(xrs_size) + "," + str(size2),
-                local_hkl
-             ]
+            if count_times == times_loop - 1:
+                print('to many "lines" in http response')
+                json_out = None
 
-            if idx >= 0 and idx < n_imgs:
-                img_lst[idx].append(dat_to_append)
+        if json_out is not None:
+            json_out = json.loads(str_lst)
 
-    #my_bar.ended()
+    except ConnectionError:
+        print("\n ConnectionError (json_data_request) \n")
+        json_out = None
 
-    return img_lst
+    except requests.exceptions.RequestException:
+        print("\n requests.exceptions.RequestException (json_data_request) \n")
+        json_out = None
 
-
-def get_refl_lst(expt_path, refl_path, img_num):
-    experiments = ExperimentListFactory.from_json_file(expt_path)
-    my_sweep = experiments.imagesets()[0]
-    print("\n my_sweep.paths               ", my_sweep.paths())
-    data_xy_flex = my_sweep.get_raw_data(0)[0].as_double()
-    print("type(data_xy_flex) =", type(data_xy_flex))
-    print("data_xy_flex.all() =", data_xy_flex.all())
-
-    print("\n refl_path =", refl_path)
-
-    table = flex.reflection_table.from_file(refl_path)
-    pos_col = list(map(list, table["xyzcal.px"]))
-    hkl_col = list(map(str, table["miller_index"]))
-    pan_col = list(map(int, table["panel"]))
-
-    n_imgs = len(my_sweep.indices())
-    pred_spt_flat_data_lst = []
-    if n_imgs > 0:
-        pred_spt_flat_data_lst = list_p_arrange(
-            pos_col, hkl_col, pan_col, n_imgs
-        )
-    return pred_spt_flat_data_lst[img_num]
+    return json_out
 
 
 if __name__ == "__main__":
-    expt_path = "/scratch/dui_tst/dui_server_run/run6/refined.expt"
-    refl_path = "/scratch/dui_tst/dui_server_run/run6/refined.refl"
-    img_num = 0
-    refl_lst = get_refl_lst(expt_path, refl_path, img_num)
+    my_cmd = {'nod_lst': [12], 'cmd_lst': ["grl 0"]}
+    json_lst = json_data_request('http://localhost:8080/', my_cmd)
+    for inner_list in json_lst[0:5]:
+        print("\n inner_list =", inner_list)
+        lst_str1 = inner_list[0].split(',')
+        print("lst_str1 =", lst_str1)
+        x_ini = float(lst_str1[0])
+        y_ini = float(lst_str1[1])
+        xrs_size = int(lst_str1[2])
+        size2 = int(lst_str1[3])
+        local_hkl = str(inner_list[1])
 
-    print("\n\n img_num[", img_num, "] (refl) = \n", refl_lst)
+        print("x_ini =     ", x_ini    )
+        print("y_ini =     ", y_ini    )
+        print("xrs_size =  ", xrs_size )
+        print("size2 =     ", size2    )
+        print("local_hkl = ", local_hkl)
+
+
+'''
+json_lst = [['850.26,578.12,3,2', '(-20, 10, -12)'], ['1258.65,582.44,3,2', '(-22, 0, -4)'], ['1006.61,656.33,3,2', '(-19, 7, -8)'], ['1200.78,655.23,3,2', '(-20, 2, -4)'], ['1306.23,670.43,3,2', '(-20, -1, -2)'], ['1091.22,673.89,3,2', '(-19, 5, -6)'], ['
+'''
 
 
