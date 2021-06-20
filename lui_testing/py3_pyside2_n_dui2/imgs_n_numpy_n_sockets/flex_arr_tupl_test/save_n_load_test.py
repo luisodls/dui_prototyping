36e1d693f93992f2bd8adabf8aa688f275cbebc9
luisolafg <luicub@gmail.com>
2021-06-20 10:24:14 +0100

    connecting better new convertion tool

--- lui_testing/py3_pyside2_n_dui2/imgs_n_numpy_n_sockets/flex_arr_tupl_test/save_n_load_test.py
+++ lui_testing/py3_pyside2_n_dui2/imgs_n_numpy_n_sockets/flex_arr_tupl_test/save_n_load_test.py
@@ -9,30 +9,21 @@ import time, zlib, json
 from dxtbx.model.experiment_list import ExperimentListFactory
 
 def build_img_arr(nrow, ncol):
-    img_arr = np.zeros((nrow, ncol), dtype=float)
-
-    for row in range(nrow):
-        for col in range(ncol):
-            img_arr[row, col] = row / 2 + col / 3
-
-    img_arr[
-        int(nrow / 4): int(nrow / 2),
-        int(ncol / 4): int(ncol * 3 / 4)
-    ] = np.max(img_arr)
-
-    img_arr[
-        int(nrow / 2): int(nrow * 3 / 4),
-        int(ncol / 4): int(ncol * 3 / 4)
-    ] = np.min(img_arr)
-    return img_arr
-
+    data_xy_flex = flex.double(flex.grid(nrow, ncol), 0)
+    for col in range(nrow):
+        for row in range(ncol):
+            i = (col + row) / 3
+            data_xy_flex[col, row] = float(i)
+    return data_xy_flex
 
 def draw_pyplot(img_arr):
     plt.imshow(img_arr, interpolation = "nearest")
     plt.show()
 
 
-def save_tup_str(np_arr):
+def save_tup_str(data_xy_flex):
+    start_tm = time.time()
+    np_arr = data_xy_flex.as_numpy_array()
     d1 = np_arr.shape[0]
     d2 = np_arr.shape[1]
     str_tup = str(tuple(np_arr.ravel()))
@@ -41,6 +32,8 @@ def save_tup_str(np_arr):
 
     byt_data = bytes(str_data.encode('utf-8'))
     compresed = zlib.compress(byt_data)
+    end_tm = time.time()
+    print("str/tuple use and compressing took ", end_tm - start_tm)
     with open("arr_img.json.zip", 'wb') as file_out:
         file_out.write(compresed)
 
@@ -62,6 +55,7 @@ def load_json_w_str():
 
 
 if __name__ == "__main__":
+    #data_xy_flex = build_img_arr(18,36)
     experiments_path = "/scratch/dui_tst/dui_server_run/run1/imported.expt"
     print("importing from:", experiments_path)
     experiments = ExperimentListFactory.from_json_file(experiments_path)
@@ -69,13 +63,10 @@ if __name__ == "__main__":
     data_xy_flex = my_sweep.get_raw_data(0)[0].as_double()
     print("type(data_xy_flex) =", type(data_xy_flex))
     print("data_xy_flex.all() =", data_xy_flex.all())
-    draw_pyplot(data_xy_flex.as_numpy_array())
 
-    #np_arr = build_img_arr(18,36)
     np_arr = data_xy_flex.as_numpy_array()
 
-    draw_pyplot(np_arr)
-    save_tup_str(np_arr)
+    save_tup_str(data_xy_flex)
     np_rec = load_json_w_str()
     draw_pyplot(np_rec)
 
