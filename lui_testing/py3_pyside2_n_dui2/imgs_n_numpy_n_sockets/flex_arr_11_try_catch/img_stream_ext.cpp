36b484de1789a46f90cf2dc0990bb5f1fa809e93
luisodls <luis.fuentes-montero@diamond.ac.uk>
2021-07-18 13:30:44 +0100

    adding test example for try & catch in C++

---
+++ lui_testing/py3_pyside2_n_dui2/imgs_n_numpy_n_sockets/flex_arr_11_try_catch/img_stream_ext.cpp
@@ -0,0 +1,96 @@
+#include <boost/python.hpp>
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <scitbx/array_family/flex_types.h>
+#include <charconv>
+#include <stdlib.h>
+#include <new>
+
+namespace py = boost::python;
+using scitbx::af::flex_double;
+using scitbx::af::flex_grid;
+using scitbx::af::flex_int;
+
+std::string img_arr_2_str(flex_double& data2d)
+
+{
+    /*
+     * Here we generate a very long string
+     * that contains and entire JSON file
+     * with the dimensions (d1, d2) and
+     * the array in "str_data"
+     */
+    int d1 = data2d.accessor().all()[0];
+    int d2 = data2d.accessor().all()[1];
+    int i, j, pos, pos_size;
+    double d_num;
+    char std_str[15];
+    int buff_size = d1 * d2 * 15 + 30;
+    std::cout << "buff_size =" << buff_size << "\n";
+    // creating a char buffer full of spaces
+    char * ch_buff;
+    ch_buff = new char[buff_size];
+    memset(ch_buff,' ',buff_size);
+    //pos will keep track of where to write next
+    pos = 0;
+    // first writing in memory string, starting with << { >>
+    strcpy(&ch_buff[pos], "{");
+    pos++;
+    // writing the << d1 >> field in JSON format, dimension #1
+    pos_size = sprintf( std_str, "\"d1\":%i,", d1);
+    strcpy(&ch_buff[pos], std_str);
+    pos = pos + pos_size;
+    // writing the << d2 >> field in JSON format, dimension #2
+    pos_size = sprintf( std_str, "\"d2\":%i,", d2);
+    strcpy(&ch_buff[pos], std_str);
+    pos = pos + pos_size;
+    // writing the left side of << str_data >> field
+    pos_size = sprintf( std_str, "\"str_data\":\"");
+    strcpy(&ch_buff[pos], std_str);
+    pos = pos + pos_size;
+
+    // all pixel intensities should be written with a loop
+    std::cout << "looping thru (" << d1 << ", " << d2 << ") ... nums \n";
+    for (i = 0; i < d1; i++) {
+        for (j = 0; j < d2; j++) {
+            // writing intensity
+            d_num = double(data2d(i, j));
+            pos_size = sprintf( std_str, "%.2f", d_num);
+            strcpy(&ch_buff[pos], std_str);
+            pos = pos + pos_size;
+            // writing coma
+            strcpy(&ch_buff[pos], ",");
+            pos++;
+        }
+    }
+    std::cout << "... Loop END\n";
+
+    // moving backwards to overwrite last coma
+    pos--;
+    // closing both: quotes and braces
+    pos_size = sprintf( std_str, "\"}");
+    strcpy(&ch_buff[pos], std_str);
+    pos = pos + pos_size;
+    // passing all char buffer to a std::string to returning it
+    std::string all_str((char *)ch_buff);
+    // clearing memory
+    delete ch_buff;
+    return all_str;
+}
+
+std::string num_2_str(int a)
+{
+    std::string all_str("Hi there");
+    int x;
+    x = 6 / a;
+    std::cout << " 6 / "<< a << " = " << x << "\n";
+    return all_str;
+}
+
+BOOST_PYTHON_MODULE(img_stream_ext)
+{
+    using namespace boost::python;
+    def("img_arr_2_str", img_arr_2_str);
+    def("num_2_str", num_2_str);
+}
---
+++ lui_testing/py3_pyside2_n_dui2/imgs_n_numpy_n_sockets/flex_arr_11_try_catch/img_stream_use.py
@@ -0,0 +1,70 @@
+import img_stream_ext
+import numpy as np
+from dials.array_family import flex
+from matplotlib import pyplot as plt
+import json
+import time
+import zlib
+from dxtbx.model.experiment_list import ExperimentListFactory
+
+
+def draw_pyplot(img_arr):
+    plt.imshow(img_arr, interpolation = "nearest")
+    plt.show()
+
+def save_json_w_str(flex_array_in):
+    start_tm = time.time()
+    str_data = img_stream_ext.img_arr_2_str(flex_array_in)
+    byt_data = bytes(str_data.encode('utf-8'))
+    byt_data = zlib.compress(byt_data)
+    end_tm = time.time()
+    print("C++ and compressing took ", end_tm - start_tm)
+    print("str_data[0:80] =", str_data[0:80])
+    print("str_data[-80:] =", str_data[-80:])
+
+    with open("arr_img.json.zip", 'wb') as file_out:
+        file_out.write(byt_data)
+
+def load_json_w_str():
+    with open("arr_img.json.zip", 'rb') as json_file:
+        compresed = json_file.read()
+
+    dic_str = zlib.decompress(compresed)
+    arr_dic = json.loads(dic_str)
+
+    d1 = arr_dic["d1"]
+    d2 = arr_dic["d2"]
+    str_data = arr_dic["str_data"]
+    print("d1, d2 =", d1, d2)
+    #print("str_data =", str_data)
+    arr_1d = np.fromstring(str_data, dtype = float, sep = ',')
+    np_array_out = arr_1d.reshape(d1, d2)
+    #print("np_array_out =\n", np_array_out)
+    return np_array_out
+
+
+if __name__ == "__main__":
+    tmp_off = '''
+    experiments_path = "/scratch/dui_tst/dui_server_run/run1/imported.expt"
+    print("importing from:", experiments_path)
+    experiments = ExperimentListFactory.from_json_file(experiments_path)
+    my_sweep = experiments.imagesets()[0]
+    data_xy_flex = my_sweep.get_raw_data(0)[0].as_double()
+    print("type(data_xy_flex) =", type(data_xy_flex))
+    print("data_xy_flex.all() =", data_xy_flex.all())
+
+    save_json_w_str(data_xy_flex)
+    print("loading json")
+    loaded_array = load_json_w_str()
+    print("drawing")
+    draw_pyplot(loaded_array)
+    '''
+    try:
+        str_data = img_stream_ext.num_2_str(2)
+
+    except:
+        print("except here")
+
+    print("str_data =", str_data)
+
+
