073580b1fc8d0f9e2156f3fb3cddd100ab1c1d92
luisodls <luis.fuentes-montero@diamond.ac.uk>
2021-08-18 19:17:12 +0100

    first prototype of dir tree view

--- lui_testing/py3_pyside2_n_dui2/tst_01/gui_tst.py
+++ lui_testing/py3_pyside2_n_dui2/tst_01/gui_tst.py
@@ -1,27 +1,49 @@
-
-import sys, time
-
+import sys, os
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
+
+def iterate(currentDir, currentItem):
+    for a_file in os.listdir(currentDir):
+        path = os.path.join(currentDir, a_file)
+        if os.path.isdir(path):
+            dirItem = QTreeWidgetItem(currentItem)
+            dirItem.setText(0, a_file)
+            iterate(path, dirItem)
+
+        else:
+            fileItem = QTreeWidgetItem(currentItem)
+            fileItem.setText(0, a_file)
+
+
+class MyTree(QTreeWidget):
+    def __init__(self, parent=None):
+        super(MyTree, self).__init__(parent=parent)
+        self.show()
+
+    def fillTree(self, startDir):
+        iterate(startDir, self)
+
 
 class Client(QDialog):
     def __init__(self, parent=None):
         super(Client, self).__init__(parent)
 
-        self.incoming_text = QTextEdit()
-        self.incoming_text.setFont(QFont("Monospace"))
+        self.t_view = MyTree()
 
         send2serverButton = QPushButton("Launch command")
         send2serverButton.clicked.connect(self.request_launch)
 
         mainLayout = QVBoxLayout()
-        mainLayout.addWidget(self.incoming_text)
+        mainLayout.addWidget(self.t_view)
         mainLayout.addWidget(send2serverButton)
         self.setLayout(mainLayout)
         self.setWindowTitle("DUI front end test with HTTP")
 
     def request_launch(self):
+        self.t_view.fillTree(
+            "/scratch/dui_prototyping/lui_testing/py3_pyside2_n_dui2"
+        )
         print("Launch \n")
 
 
