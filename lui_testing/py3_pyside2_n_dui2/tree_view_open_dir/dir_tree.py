0f916ff4ce077160db879d1bbc90e5d1836321b0
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-03-22 18:53:32 +0000

    coding on test example for tree view

--- lui_testing/py3_pyside2_n_dui2/tree_view_open_dir/dir_tree.py
+++ lui_testing/py3_pyside2_n_dui2/tree_view_open_dir/dir_tree.py
@@ -1,12 +1,4 @@
-'''
-model = QFileSystemModel()
-model.setRootPath(QDir.currentPath())
 
-tree =  QTreeView()
-tree.setModel(model)
-
-######################################################################################
-'''
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2 import QtUiTools
@@ -22,13 +14,28 @@ class MainObject(QObject):
         self.window = QtUiTools.QUiLoader().load(ui_path)
         self.window.setWindowTitle("CCP4 DUI Cloud")
 
-        model = QFileSystemModel()
-        model.setRootPath(QDir.currentPath())
-        tree =  QTreeView()
-        tree.setModel(model)
-        self.window.verticalLayout.addWidget(tree)
+        self.mod = QFileSystemModel()
+        self.mod.setRootPath(QDir.homePath())
 
+        self.tree_vw =  QTreeView()
+        self.tree_vw.setModel(self.mod)
+        self.window.verticalLayout.addWidget(self.tree_vw)
+
+        self.tree_vw.clicked.connect(self.someting_click)
         self.window.show()
+
+    def someting_click(self, event):
+        index = self.tree_vw.currentIndex()
+        #print("\n\n dir(self.tree_vw) = ", dir(self.tree_vw), "\n")
+        print("\n\n index =", index)
+        #print("dir(self.mod) = ", dir(self.mod))
+        new_path = self.mod.filePath(index)
+        new_name = self.mod.fileName(index)
+        new_info = self.mod.fileInfo(index)
+        print("new_path =", new_path)
+        print("new_name =", new_name)
+        print("new_info.isDir =", new_info.isDir())
+        print("new_info.isFile =", new_info.isFile())
 
 if __name__ == "__main__":
 
