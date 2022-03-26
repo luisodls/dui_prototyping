66a28902906c807dc77fa9d67ebafc906e100ad7
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-03-26 17:45:32 +0000

    tesing html web view on arm toy

--- lui_testing/py3_pyside2_n_dui2/html_view/test01.py
+++ lui_testing/py3_pyside2_n_dui2/html_view/test01.py
@@ -2,6 +2,9 @@ import sys
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
+
+from PySide2.QtWebEngineWidgets import QWebEngineView
+
 from PySide2 import QtUiTools
 
 
@@ -123,22 +126,17 @@ class Form(QObject):
         super(Form, self).__init__(parent)
         self.window = QtUiTools.QUiLoader().load("simple.ui")
 
-        self.num = 0
+        self.window.Button1.clicked.connect(self.clicked)
+        self.html_view = QWebEngineView()
 
-        #Next line is NOT doing what I wanted, to return data with connect
-        self.data_test = self.window.Button1.clicked.connect(self.clicked)
-
-        self.window.PrintDataButton.clicked.connect(self.imprime)
+        main_box = QVBoxLayout()
+        main_box.addWidget(self.html_view)
+        self.window.HtmlWidget.setLayout(main_box)
         self.window.show()
 
     def clicked(self):
         print("clicked")
-        self.num += 1
-        print("self.num =", self.num)
-        return int(self.num)
-
-    def imprime(self):
-        print("self.data_test =", self.data_test)
+        self.html_view.load(QUrl("https://google.com"))
 
 
 if __name__ == '__main__':
