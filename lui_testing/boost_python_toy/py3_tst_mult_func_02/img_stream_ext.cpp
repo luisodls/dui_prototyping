bc6cb9f72e60b86ab18a2f2417cdbb61fc863ca2
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-05-28 16:07:30 +0100

    updating boost.python tests

--- lui_testing/boost_python_toy/py3_tst_mult_func_02/img_stream_ext.cpp
+++ lui_testing/boost_python_toy/py3_tst_mult_func_02/img_stream_ext.cpp
@@ -14,119 +14,38 @@ char const* greet()
     return "hello, world";
 }
 
-
 std::string cadena1()
 {
     std::string std_str = "Aa Bb";
     return std_str;
 }
 
-py::list lst_bunch(flex_double& data2d)
+std::string build_str(flex_double& data2d)
 {
-    py::list data_out;
-    std::string std_str = "";
-    int nrow = data2d.accessor().all()[0];
-    int ncol = data2d.accessor().all()[1];
-    int col, row;
+    char std_str[15];
+    std::cout << "dim 0 size =" << data2d.accessor().all()[0] << "\n" <<
+                 "dim 1 size =" << data2d.accessor().all()[1] << "\n";
 
-    std::cout << "nrow = " << nrow << "\n";
-    std::cout << "ncol = " << ncol << "\n";
-    for (row = 0; row < nrow; row++) {
-        for (col = 0; col < ncol; col++) {
-            std_str.append(
-                boost::lexical_cast<std::string>(data2d(row, col))
-            );
-            std_str.append(",");
-        }
-    }
-    data_out.append(std_str);
+    int x_size = data2d.accessor().all()[0];
+    int y_size = data2d.accessor().all()[1];
 
-    return data_out;
+    int buff_size = x_size * y_size * 10;
+    char ch_buff[buff_size];
+    memset(ch_buff,'f',buff_size);
+
+    std::string result;
+
+    //std::cout << "ch_buff =" << ch_buff << "\n";
+    result = std::string(ch_buff, buff_size);
+    //std::cout << "result =" << result << "\n";
+
+    return result;
 }
-
-
-py::list arange_list(py::list bbox_lst, py::list hkl_lst, int n_imgs)
-{
-    /*
-     * This is the function that we actually need.
-     *
-     * from a list of shoe - box bounds
-     * it generates a new list of reflections arranged
-     * per image
-     */
-    std::cout << "n_imgs =" << n_imgs << "\n";
-
-    int x_ini, y_ini, width, height;
-    py::list img_lst, ref_box, tmp_lst, box_dat;
-
-    //TODO make sure there is no way to avoid this loop
-    for (int i = 0; i < n_imgs; i++){
-        img_lst.append(py::list());
-    }
-    py::str local_hkl;
-
-    for (int i = 0; i < len(bbox_lst); i++){
-        ref_box = py::extract<py::list>(bbox_lst[i]);
-        x_ini = py::extract<int>(ref_box[0]);
-        y_ini = py::extract<int>(ref_box[2]);
-        width = py::extract<int>(ref_box[1]) - py::extract<int>(ref_box[0]);
-        height = py::extract<int>(ref_box[3]) - py::extract<int>(ref_box[2]);
-
-        box_dat = py::list();
-        box_dat.append(x_ini);
-        box_dat.append(y_ini);
-        box_dat.append(width);
-        box_dat.append(height);
-
-
-        if( len(hkl_lst) <= 1 ){
-            local_hkl = "";
-            box_dat.append(local_hkl);
-        } else {
-            //box_dat.append(py::extract<std::string>(hkl_lst[i]));
-            local_hkl = py::extract<py::str>(hkl_lst[i]);
-            if(local_hkl == "(0, 0, 0)"){
-                local_hkl = "NO Index";
-            }
-            box_dat.append(local_hkl);
-        }
-
-        for (int idx = py::extract<int>(ref_box[4]);
-             idx < py::extract<int>(ref_box[5]);
-             idx++){
-            tmp_lst = py::extract<py::list>(img_lst[idx]);
-            tmp_lst.append(box_dat);
-        }
-    }
-
-    return img_lst;
-}
-
-std::string add_elem_as_str(flex_double& data2d)
-{
-    std::string std_str = "";
-    int nrow = data2d.accessor().all()[0];
-    int ncol = data2d.accessor().all()[1];
-    int col, row, tot = 0;
-
-    std::cout << "nrow = " << nrow << "\n";
-    std::cout << "ncol = " << ncol << "\n";
-    for (row = 0; row < nrow; row++) {
-        for (col = 0; col < ncol; col++) {
-            tot = tot + data2d(row, col);
-        }
-    }
-    std_str.append(boost::lexical_cast<std::string>(tot));
-    return std_str;
-}
-
 
 BOOST_PYTHON_MODULE(img_stream_ext)
 {
     using namespace boost::python;
     def("greet", greet);
     def("cadena1", cadena1);
-    def("lst_bunch", lst_bunch);
-    def("arange_list", arange_list);
-    def("add_elem_as_str", add_elem_as_str);
+    def("build_str", build_str);
 }
--- lui_testing/boost_python_toy/py3_tst_mult_func_02/img_stream_use.py
+++ lui_testing/boost_python_toy/py3_tst_mult_func_02/img_stream_use.py
@@ -2,21 +2,25 @@ import img_stream_ext
 import numpy as np
 from dials.array_family import flex
 
+import resource, sys
+resource.setrlimit(resource.RLIMIT_STACK, (2**29,-1))
+sys.setrecursionlimit(10**6)
+
 print(img_stream_ext.greet())
 
-x_max = 6
-y_max = 3
+x_max = 4000
+y_max = 3000
+print("building data start")
 data_xyz_flex = flex.double(flex.grid(x_max, y_max), 0)
 for x in range(x_max):
     for y in range(y_max):
         i = (x + y) / 2
         data_xyz_flex[x, y] = float(i)
+print("building data end")
 
 print("data_xyz_flex = \n", data_xyz_flex.as_numpy_array())
-
-tst = img_stream_ext.lst_bunch(data_xyz_flex)
-
-print("tst =", tst)
 print("cadena1 =", img_stream_ext.cadena1())
-tot = img_stream_ext.add_elem_as_str(data_xyz_flex)
-print("tot =", tot)
+print("here 1")
+str_tst = img_stream_ext.build_str(data_xyz_flex)
+print("here 2")
+print("len(str_tst) =", len(str_tst), ">>")
