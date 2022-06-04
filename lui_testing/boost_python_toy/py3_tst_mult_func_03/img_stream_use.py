1197653f4755893a5b1bc1c8649411b6b5323085
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-06-04 21:34:18 +0100

    tunning array size and error output

--- lui_testing/boost_python_toy/py3_tst_mult_func_03/img_stream_use.py
+++ lui_testing/boost_python_toy/py3_tst_mult_func_03/img_stream_use.py
@@ -8,8 +8,8 @@ from dials.array_family import flex
 
 import resource, sys
 
-x_max = 13
-y_max = 14
+x_max = 3000
+y_max = 3000
 print("building data start")
 data_xyz_flex = flex.double(flex.grid(x_max, y_max), 0)
 for x in range(x_max):
@@ -19,7 +19,7 @@ for x in range(x_max):
 print("building data end")
 
 print("____________________________________________________________")
-inv_scl, x1, y1, x2, y2 = 2, 1, 2, 10, 11
+inv_scl, x1, y1, x2, y2 = 7, 4, 5, 2600, 2700
 
 py_start_tm = time.time()
 py_str_tst = img_stream_py.slice_arr_2_str(
@@ -30,7 +30,6 @@ print("scaling and converting in Python took ", py_end_tm - py_start_tm)
 print(
     "py_str_tst ... =", py_str_tst[:70], "  ....  ", py_str_tst[-70:]
 )
-
 
 cpp_start_tm = time.time()
 cpp_str_tst = img_stream_ext.slice_arr_2_str(
