903b2286e668603900ac7825c745443496324b49
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-06-06 15:14:40 +0100

    metastable phase before starting to tweak for scaling the mask

--- lui_testing/boost_python_toy/py3_tst_bool_mask_01/img_stream_use.py
+++ lui_testing/boost_python_toy/py3_tst_bool_mask_01/img_stream_use.py
@@ -8,17 +8,19 @@ from dials.array_family import flex
 
 import resource, sys
 
-x_max = 3000
-y_max = 3000
+x_max = 10
+y_max = 10
 print("building data start")
 data_bool_flex = flex.bool(flex.grid(x_max, y_max), 0)
 for x in range(x_max):
     for y in range(y_max):
-        if float(x+y) / 2.0 == int((x+y)/2):
+        if x > y:
             data_bool_flex[x, y] = True
 
         else:
             data_bool_flex[x, y] = False
+
+print("data in =\n", data_bool_flex.as_numpy_array())
 
 cpp_start_tm = time.time()
 cpp_str_tst = img_stream_ext.mask_arr_2_str(data_bool_flex)
@@ -33,9 +35,9 @@ print("converting in C++ took ", cpp_end_tm - cpp_start_tm)
 print("converting in Python took ", py_end_tm - py_start_tm)
 
 print("len(cpp_str_tst) =", len(cpp_str_tst))
-#print( "cpp_str_tst =", cpp_str_tst)
-print( "cpp_str_tst =", cpp_str_tst[:50], "  ...  ", cpp_str_tst[-50:])
+print( "cpp_str_tst =", cpp_str_tst)
+#print( "cpp_str_tst =", cpp_str_tst[:50], "  ...  ", cpp_str_tst[-50:])
 print("len(py_str_tst) =", len(py_str_tst))
-#print( "py_str_tst  =", py_str_tst)
-print( "py_str_tst  =", py_str_tst[:50], "  ...  ", py_str_tst[-50:])
+print( "py_str_tst  =", py_str_tst)
+#print( "py_str_tst  =", py_str_tst[:50], "  ...  ", py_str_tst[-50:])
 
