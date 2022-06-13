907ae235e562054139c8490d982d6a217660cd3c
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-06-13 21:20:55 +0100

    starting to test multiple threads

---
+++ lui_testing/py3_pyside2_n_dui2/subprocess_n_connections/multi_proc01.py
@@ -0,0 +1,13 @@
+from multiprocessing import Process, Pipe
+import time
+def func(conn, num):
+    num_times_2 = num * 2
+    conn.send(num_times_2)
+    conn.close()
+
+if __name__ == '__main__':
+    conn01, conn02 = Pipe()
+    p = Process(target=func, args=(conn02, 5))
+    p.start()
+    print(conn01.recv())
+    p.join()
