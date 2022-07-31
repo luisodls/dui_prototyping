46f9bbf6c789a399a22b3f5a7cdc618cf75a374c
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-07-31 22:23:40 +0100

    test

--- lui_testing/py3_pyside2_n_dui2/micelaneous/path_tst.py
+++ lui_testing/py3_pyside2_n_dui2/micelaneous/path_tst.py
@@ -1,6 +1,6 @@
-import os
+import os, shutil
 
-if __name__ == "__main__":
+    to_test = '''
     my_tst_path = os.environ["PWD"]
     print("my_tst_path =", my_tst_path, "\n")
     print("os.get_exec_path =", os.get_exec_path())
@@ -8,3 +8,21 @@ if __name__ == "__main__":
     print("os.path.abspath =", os.path.abspath("."))
     print("os.getcwd =", os.getcwd())
     print("os.sep =", os.sep)
+    '''
+
+
+def create_dir():
+    path2add = os.getcwd() + os.sep + "here"
+    try:
+        shutil.rmtree(path2add)
+
+    except FileNotFoundError:
+        print("No need to remove non existent dir")
+
+    os.mkdir(path2add)
+    return path2add
+
+if __name__ == "__main__":
+
+    new_dir = create_dir()
+    print("new_dir =", new_dir)
