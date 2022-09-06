14a582bae0266a64796011379d8659963637a266
rober-loxl <robert.loxley.i7@gmail.com>
2022-09-06 11:29:09 +0100

    small fix on run_me.bat test

--- lui_testing/win10_bat/tst01.bat
+++ lui_testing/win10_bat/tst01.bat
@@ -1,3 +1,4 @@
 set ini_dir=%cd%
-set full_sting=python %ini_dir%\DUI2\src\all_local.py windows_exe=true
+set full_sting=python %ini_dir%\src\all_local.py windows_exe=true
 echo %full_sting% > run_me.bat
+echo: use run_me.bat to launch Dui2
