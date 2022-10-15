e593add787889a15d9d4ce3f0af7b9cf6d973173
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-10-15 23:32:07 +0100

    playing with multitasting toy test

--- lui_testing/py3_pyside2_n_dui2/multi_thread/multi_thread_01.py
+++ lui_testing/py3_pyside2_n_dui2/multi_thread/multi_thread_01.py
@@ -12,20 +12,18 @@ class my_one(QThread):
         print("")
 
 
-#RunPushButton
-
-
 class MainImgViewObject(QObject):
     def __init__(self, parent = None):
         super(MainImgViewObject, self).__init__(parent)
         self.parent_app = parent
-
         self.window = QtUiTools.QUiLoader().load("main.ui")
         self.window.setWindowTitle("test")
-
         print("inside QObject")
-
+        self.window.RunPushButton.clicked.connect(self.run_one_clicked)
         self.window.show()
+
+    def run_one_clicked(self):
+        print("run_one_clicked(MainImgViewObject)")
 
 
 def main():
@@ -34,6 +32,7 @@ def main():
     print("before sys.exit")
     sys.exit(app.exec_())
     print("after sys.exit")
+
 
 if __name__ == "__main__":
     main()
