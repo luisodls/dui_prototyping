aed03114c66e14ad623e4ebb7e30a851d0b16f45
luisodls <luis.fuentes-montero@diamond.ac.uk>
2022-10-16 12:42:23 +0100

    playing with multitasting toy test, adding list of threads

--- lui_testing/py3_pyside2_n_dui2/multi_thread/multi_thread_01.py
+++ lui_testing/py3_pyside2_n_dui2/multi_thread/multi_thread_01.py
@@ -1,15 +1,19 @@
-import sys, os
+import sys, os, time
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from PySide2 import QtUiTools
 
 class my_one(QThread):
-    def __init__(self):
+    def __init__(self, times_in = 3):
         super(my_one, self).__init__()
+        self.times = times_in
 
     def run(self):
-        print("")
+        print("times entered =", self.times)
+        for rept in range(self.times):
+            print("rep n", rept, " of ", self.times)
+            time.sleep(1)
 
 
 class MainImgViewObject(QObject):
@@ -21,17 +25,19 @@ class MainImgViewObject(QObject):
         print("inside QObject")
         self.window.RunPushButton.clicked.connect(self.run_one_clicked)
         self.window.show()
+        self.thread_lst = []
 
     def run_one_clicked(self):
         print("run_one_clicked(MainImgViewObject)")
+        new_thread = my_one(len(self.thread_lst) + 1)
+        new_thread.start()
+        self.thread_lst.append(new_thread)
 
 
 def main():
     app = QApplication(sys.argv)
     m_obj = MainImgViewObject(parent = app)
-    print("before sys.exit")
     sys.exit(app.exec_())
-    print("after sys.exit")
 
 
 if __name__ == "__main__":
