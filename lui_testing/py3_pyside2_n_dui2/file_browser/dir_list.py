e6417f02bb3f0428cf3e247fd291568c2e7ec957
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2023-05-19 15:41:13 +0100

    organizing list view test

--- lui_testing/py3_pyside2_n_dui2/file_browser/dir_list.py
+++ lui_testing/py3_pyside2_n_dui2/file_browser/dir_list.py
@@ -6,6 +6,21 @@ from PySide2.QtGui import *
 
 import os, sys
 
+class MyDirView(QListWidget):
+    def __init__(self, parent = None):
+        super(MyDirView, self).__init__(parent)
+        self.itemClicked.connect(self.someting_click)
+
+    def add_dummy_item(self, str_in, num):
+        tst_item = QListWidgetItem(str_in)
+        tst_item.setIcon(QIcon("../icon_tst/icon_resources/import.png"))
+        tst_item.tst_num = 50 - num
+        self.addItem(tst_item)
+
+    def someting_click(self, item):
+        print("tst_num =", item.tst_num)
+
+
 class MainObject(QObject):
     def __init__(self, parent = None):
         super(MainObject, self).__init__(parent)
@@ -14,22 +29,12 @@ class MainObject(QObject):
         self.window = QtUiTools.QUiLoader().load(ui_path)
         self.window.setWindowTitle("CCP4 DUI Cloud")
 
-        self.lst_vw =  QListWidget()
+        self.lst_vw =  MyDirView()
+        for nm in range(10):
+            self.lst_vw.add_dummy_item("a" * nm, nm)
+
         self.window.verticalLayout.addWidget(self.lst_vw)
-
-        self.lst_vw.itemClicked.connect(self.someting_click)
         self.window.show()
-
-    def someting_click(self, item):
-        print("\n item =", item)
-        print("tst_num =", item.tst_num)
-
-    def add_something_dummy(self, str_in, num):
-        tst_item = QListWidgetItem(str_in)
-        tst_item.setIcon(QIcon("../icon_tst/icon_resources/import.png"))
-        tst_item.tst_num = 50 - num
-        self.lst_vw.addItem(tst_item)
-
 
 
 if __name__ == "__main__":
@@ -37,8 +42,5 @@ if __name__ == "__main__":
     QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QApplication(sys.argv)
     m_obj = MainObject()
-
-    for nm in range(10):
-        m_obj.add_something_dummy("a" * nm, nm)
 
     sys.exit(app.exec_())
