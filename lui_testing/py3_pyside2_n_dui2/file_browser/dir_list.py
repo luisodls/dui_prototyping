09c3e2e4e5059ca7859d820d3663e493e61f2963
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2023-05-19 14:01:54 +0100

    testing adding to list

--- lui_testing/py3_pyside2_n_dui2/file_browser/dir_list.py
+++ lui_testing/py3_pyside2_n_dui2/file_browser/dir_list.py
@@ -17,20 +17,29 @@ class MainObject(QObject):
         self.mod = QFileSystemModel()
         self.mod.setRootPath(QDir.homePath())
 
-        self.tree_vw =  QListWidget()
-        self.tree_vw.addItem("AAAAAAAAA")
+        self.lst_vw =  QListWidget()
+        self.window.verticalLayout.addWidget(self.lst_vw)
 
-        self.window.verticalLayout.addWidget(self.tree_vw)
-
-        self.tree_vw.clicked.connect(self.someting_click)
+        self.lst_vw.clicked.connect(self.someting_click)
         self.window.show()
 
     def someting_click(self, event):
         print("\n\n event =", event)
+
+    def add_something_dummy(self, str_in):
+        tst_item = QListWidgetItem(str_in)
+        tst_item.setIcon(QIcon("../icon_tst/icon_resources/import.png"))
+        self.lst_vw.addItem(tst_item)
+
+
 
 if __name__ == "__main__":
 
     QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
     app = QApplication(sys.argv)
     m_obj = MainObject()
+
+    for nm in range(10):
+        m_obj.add_something_dummy("a" * nm)
+
     sys.exit(app.exec_())
