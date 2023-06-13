a9c070212e668d167a928d4ad4c5355d324a96b0
luisodls <luis.fuentes-montero@diamond.ac.uk>
2023-06-13 15:09:15 +0100

    creating an example for multithreading httpS requests

---
+++ lui_testing/py3_pyside2_n_dui2/http_tst/httpS_test_02/client.py
@@ -0,0 +1,27 @@
+import requests
+import time
+
+command = str(input(">> "))
+print('entered:', command)
+
+cmd = {'command': [command]}
+
+r_g = requests.get(
+    'https://localhost:8080/', stream = True,
+    params = cmd, verify = False
+)
+
+line_str = ''
+while True:
+    tmp_dat = r_g.raw.read(1)
+    single_char = str(tmp_dat.decode('utf-8'))
+    line_str += single_char
+    if single_char == '\n':
+        print('receiving <<', line_str, '>>')
+        line_str = ''
+
+    elif line_str[-7:] == '/*EOF*/':
+        print('/*EOF*/ received')
+        break
+
+    print('#', end = '')
---
+++ lui_testing/py3_pyside2_n_dui2/http_tst/httpS_test_02/server.py
@@ -0,0 +1,57 @@
+#import http.server, socketserver
+from urllib.parse import urlparse, parse_qs
+import time
+from http.server import HTTPServer, BaseHTTPRequestHandler, socketserver
+import ssl
+
+class ReqHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        url_path = self.path
+        print('urlparse(self.path).query', parse_qs(urlparse(url_path).query))
+
+        self.wfile.write(bytes('Test #4\n', 'utf-8'))
+        self.wfile.write(bytes('1234567890\n', 'utf-8'))
+        self.wfile.write(bytes('12345678901234567890\n', 'utf-8'))
+        self.wfile.write(bytes('123456789012345678901234567890\n', 'utf-8'))
+
+        print("starting to send numbers ...")
+        for num in range(9):
+            num_str = ' num = ' + str(num) + '\n'
+            time.sleep(0.5)
+            print("sending <<", num_str, ">> str ")
+            self.wfile.write(bytes(num_str, 'utf-8'))
+
+        print("... finished sending numbers")
+        print("sending /*EOF*/")
+        self.wfile.write(bytes('/*EOF*/', 'utf-8'))
+
+
+if __name__ == "__main__":
+    PORT = 8080
+
+    with socketserver.ThreadingTCPServer(
+        ("", PORT), ReqHandler
+    ) as http_daemon:
+
+        context = ssl.SSLContext()
+        context.load_cert_chain(
+            "../httpS_test_01/testeando.pem", "../httpS_test_01/testeando.key"
+        )
+        http_daemon.socket = context.wrap_socket(http_daemon.socket)
+
+        print("serving at port", PORT, "with:ThreadingTCPServer")
+        http_daemon.serve_forever()
+
+    to_remove = '''
+    try:
+        http_daemon.serve_forever()
+
+    except KeyboardInterrupt:
+        print("\n Keyboard-Interrupt received")
+        http_daemon.server_close()
+    '''
+
