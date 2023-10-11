bf9739e6aab1a901520592fb84edbea087783481
luisodls <luis.fuentes-montero@diamond.ac.uk>
2023-10-11 18:10:19 +0100

    fixing path for a test with generate mask

--- lui_testing/test_scripts/run_mask_tst_rectangle.sh
+++ lui_testing/test_scripts/run_mask_tst_rectangle.sh
@@ -1,4 +1,5 @@
-dials.import input.template="/home/luiso/dif_dat/I23_only_3_imgs_twise/test_3_####.cbf"
-dials.generate_mask /tmp/run_dui2_nodes/run1/imported.expt untrusted.rectangle=909,1692,35,132 output.mask=tmp_mask.pickle
+#dials.import input.template="/home/luiso/dif_dat/I23_only_3_imgs_twise/test_3_####.cbf"
+dials.import input.template="/home/lui/dif_dat/I23_only_3_imgs_twise/test_3_####.cbf"
+dials.generate_mask imported.expt untrusted.rectangle=909,1692,35,132 output.mask=tmp_mask.pickle
 dials.apply_mask imported.expt input.mask=tmp_mask.pickle
 dials.image_viewer masked.expt
