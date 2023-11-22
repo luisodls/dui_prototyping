12f8836b44bdb6f738269a13f5e6cdf4dc35e988
luisodls <luis.fuentes-montero@diamond.ac.uk>
2023-11-22 20:34:40 +0000

    duplication num 5 for toml file test

---
+++ lui_testing/toml_tests/tst_n_05/src/hello.py
@@ -0,0 +1,8 @@
+from writing import imprime_algo
+
+def main():
+    imprime_algo("Hi there #1")
+
+
+if __name__ == "__main__":
+    main()
---
+++ lui_testing/toml_tests/tst_n_05/src/in_dir/write_from_dir.py
@@ -0,0 +1,7 @@
+def imprime_algo_mas(str_out):
+    print(str_out)
+
+
+if __name__ == "__main__":
+    imprime_algo_mas("Hi there #3")
+
