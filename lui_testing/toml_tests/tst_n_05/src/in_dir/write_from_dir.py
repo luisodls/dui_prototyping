931a5cfcbe33631381f76eaf1f1856fb9523c6a0
luisodls <luis.fuentes-montero@diamond.ac.uk>
2023-11-23 14:32:04 +0000

    some test 1/2 way thru

--- lui_testing/toml_tests/tst_n_05/src/in_dir/write_from_dir.py
+++ lui_testing/toml_tests/tst_n_05/src/in_dir/write_from_dir.py
@@ -1,7 +1,15 @@
+import os
+
 def imprime_algo_mas(str_out):
     print(str_out)
+
+def lee_un_fichero():
+    print("os.path.dirname(os.path.abspath(__file__))", os.path.dirname(os.path.abspath(__file__) ) )
+
+    print("leyendo")
 
 
 if __name__ == "__main__":
     imprime_algo_mas("Hi there #3")
+    lee_un_fichero()
 
