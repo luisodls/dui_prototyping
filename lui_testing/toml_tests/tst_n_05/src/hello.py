4bb1c0a5bfa4c3903e39dcb318ab59d83dc68f96
luisodls <luis.fuentes-montero@diamond.ac.uk>
2023-11-23 14:58:10 +0000

    adding lecture from disc in install dir

--- lui_testing/toml_tests/tst_n_05/src/hello.py
+++ lui_testing/toml_tests/tst_n_05/src/hello.py
@@ -1,7 +1,9 @@
 from writing import imprime_algo
+from in_dir.write_from_dir import lee_un_fichero
 
 def main():
     imprime_algo("Hi there #1")
+    lee_un_fichero()
 
 
 if __name__ == "__main__":
--- lui_testing/toml_tests/tst_n_05/src/in_dir/write_from_dir.py
+++ lui_testing/toml_tests/tst_n_05/src/in_dir/write_from_dir.py
@@ -4,9 +4,15 @@ def imprime_algo_mas(str_out):
     print(str_out)
 
 def lee_un_fichero():
-    print("os.path.dirname(os.path.abspath(__file__))", os.path.dirname(os.path.abspath(__file__) ) )
+    abs_path = str(os.path.dirname(os.path.abspath(__file__)))
+    print("abs_path =", abs_path)
+    resr_path = abs_path + "/res_txt/data.txt"
+    print("leyendo contenido de:", resr_path)
 
-    print("leyendo")
+    fil = open(resr_path, "r")
+    str_file = fil.read()
+    fil.close()
+    print("str from file =", str_file)
 
 
 if __name__ == "__main__":
