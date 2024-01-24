403cb5227ce205c4f1748c0e2aca91ef4391260b
luisodls <luis.fuentes-montero@diamond.ac.uk>
2024-01-24 19:04:04 +0000

    more testing with tobyte bufering

--- lui_testing/py3_pyside2_n_dui2/imgs_n_numpy_n_sockets/numpy2stream/tst_02_tmpfile.py
+++ lui_testing/py3_pyside2_n_dui2/imgs_n_numpy_n_sockets/numpy2stream/tst_02_tmpfile.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 import tempfile
 
 def build_img_arr(nrow, ncol):
-    img_arr = np.arange(nrow * ncol).reshape(nrow, ncol)
+    img_arr = np.arange(nrow * ncol, dtype = float).reshape(nrow, ncol)
     img_arr[
         int(nrow / 4): int(nrow / 2),
         int(ncol / 4): int(ncol * 3 / 4)
@@ -13,34 +13,32 @@ def build_img_arr(nrow, ncol):
         int(nrow / 2): int(nrow * 3 / 4),
         int(ncol / 4): int(ncol * 3 / 4)
     ] = np.min(img_arr)
+
+    print("\nimg_arr =\n", img_arr)
     return img_arr
 
 def draw_pyplot(img_arr):
+    print("\nimg_arr =\n",img_arr)
     plt.imshow(img_arr, interpolation = "nearest")
     plt.show()
 
 def save_np_array(np_array_in):
-    np.save(
-        tmp_fil, np_array_in,
-        allow_pickle = False, fix_imports = False
-    )
+    return np_array_in.tobytes(order='C')
 
-def load_np_array():
-    img_arr_load = np.load(
-        tmp_fil, mmap_mode = None, allow_pickle = False,
-        fix_imports = False, encoding = 'ASCII'
-    )
+def load_np_array(arr_bit):
+    arr_1d = np.frombuffer(arr_bit, dtype = float)
+    img_arr_load = arr_1d.reshape(8, 12)
     return img_arr_load
 
 if __name__ == "__main__":
     tmp_fil = tempfile.TemporaryFile()
 
-    ncol = 30
-    nrow = 70
+    nrow = 8
+    ncol = 12
     img_arr = build_img_arr(nrow, ncol)
 
-    save_np_array(img_arr)
+    bin_dat = save_np_array(img_arr)
     tmp_fil.seek(0)
-    loaded_array = load_np_array()
+    loaded_array = load_np_array(bin_dat)
 
     draw_pyplot(loaded_array)
