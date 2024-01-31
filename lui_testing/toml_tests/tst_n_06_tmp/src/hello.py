ad8884035b6be454b083cd51f5368950e492e092
luisodls <luis.fuentes-montero@diamond.ac.uk>
2024-01-31 14:42:38 +0000

    adding temp test dir

---
+++ lui_testing/toml_tests/tst_n_06_tmp/src/hello.py
@@ -0,0 +1,10 @@
+from writing import imprime_algo
+from in_dir.write_from_dir import lee_un_fichero
+
+def main():
+    imprime_algo("Hi there #1")
+    lee_un_fichero()
+
+
+if __name__ == "__main__":
+    main()
