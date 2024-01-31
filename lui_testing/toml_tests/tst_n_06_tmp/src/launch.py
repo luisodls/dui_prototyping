77ccb96aacc4e7a957c0f5ad0c0c1802e7290af8
luisodls <luis.fuentes-montero@diamond.ac.uk>
2024-01-31 15:03:05 +0000

    incremental change n2 ... adding dir with rec and GUI code

--- lui_testing/toml_tests/tst_n_06_tmp/src/launch.py
+++ lui_testing/toml_tests/tst_n_06_tmp/src/launch.py
@@ -1,11 +1,44 @@
 from writing import imprime_algo
 from in_dir.write_from_dir import lee_un_fichero
 
+import sys, os
+from PySide2.QtCore import *
+from PySide2.QtWidgets import *
+from PySide2.QtGui import *
+
+from PySide2.QtWebEngineWidgets import QWebEngineView
+from PySide2 import QtUiTools
+
+class Form(QObject):
+    def __init__(self, parent = None):
+        super(Form, self).__init__(parent)
+        self.window = QtUiTools.QUiLoader().load("res1/simple.ui")
+
+        self.window.Button1.clicked.connect(self.clicked)
+        self.html_view = QWebEngineView()
+
+        main_box = QVBoxLayout()
+        main_box.addWidget(self.html_view)
+
+        self.window.InerWidget.setLayout(main_box)
+        self.window.show()
+
+    def clicked(self):
+        print("clicked")
+        abs_path = str(os.path.dirname(os.path.abspath(__file__)))
+        html_path = abs_path + "/in_dir/res_txt/dummy.html"
+        print("html_path =", html_path)
+
+        self.html_view.load(QUrl.fromLocalFile(html_path))
+        self.html_view.show()
+
+
 def main():
-    print("tst 6 tmp")
-    imprime_algo("Hi there #1")
-    lee_un_fichero()
+    print("tst 7 tmp")
+    app = QApplication(sys.argv)
+    form = Form()
+    sys.exit(app.exec_())
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
