d75052138e09f4dfa00145b90a91b3187cf20280
luisodls <luis.fuentes-montero@diamond.ac.uk>
2024-07-26 16:59:05 +0100

    adding dials and image use to new test toy

--- lui_testing/py3_pyside2_n_dui2/imgs_n_numpy_n_sockets/nxs_test/qthreads_n_imgs/server.py
+++ lui_testing/py3_pyside2_n_dui2/imgs_n_numpy_n_sockets/nxs_test/qthreads_n_imgs/server.py
@@ -3,6 +3,36 @@ import socketserver
 from urllib.parse import urlparse, parse_qs
 import time, subprocess
 
+from dxtbx.model.experiment_list import ExperimentListFactory
+from dxtbx.flumpy import to_numpy
+
+
+def get_experiments(experiment_path):
+    print("importing from:" + experiment_path)
+    for repeat in range(10):
+        try:
+            new_experiments = ExperimentListFactory.from_json_file(
+                experiment_path
+            )
+            break
+
+        except OSError:
+            new_experiments = None
+            print("OS Err catch in ExperimentListFactory, trying again")
+            time.sleep(0.333)
+
+    return new_experiments
+
+def image_loading(img_num):
+    experiments = get_experiments("imported.expt")
+    my_sweep = experiments.imagesets()[0]
+    raw_dat = my_sweep.get_raw_data(img_num)
+    data_xy_flex = raw_dat[0].as_double()
+    np_arr2 = to_numpy(data_xy_flex)
+    txt_slice = str(np_arr2[50:90,40:80])
+    return txt_slice
+
+
 class ReqHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
@@ -19,8 +49,9 @@ class ReqHandler(http.server.BaseHTTPRequestHandler):
         str_2_send = 'num_of_imgs =' + str(num_of_imgs)
         print(str_2_send)
 
-        for repets in range(num_of_imgs):
-            str_n_X = 'img num ' + str(repets) + ' \n'
+        for img_num in range(num_of_imgs):
+            img_slice = image_loading(img_num)
+            str_n_X = img_slice + ' \n'
             self.wfile.write(bytes(str_n_X, 'utf-8'))
 
         print("sending /*EOF*/")
