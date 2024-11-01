bfe76b7222808ae568e31562190191d141cadc66
luisodls <luis.fuentes-montero@@diamond.ac.uk>
2024-11-01 15:41:31 +0000

    connecting something to the buttons

--- lui_testing/cpp_qt_toys/tst_demo/fit_n2/mainwindow.cpp
+++ lui_testing/cpp_qt_toys/tst_demo/fit_n2/mainwindow.cpp
@@ -1,5 +1,11 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
+
+//#include <QApplication>
+#include <QFileDialog>
+#include <QString>
+
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -11,5 +17,19 @@ MainWindow::MainWindow(QWidget *parent)
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_ini_clicked()
+{
+    QString  file1Name ;
+    file1Name = QFileDialog::getOpenFileName(this,
+             tr("Open File 1"), "/home", tr("all Files (*.*)"));
+        ui->GraphPath->setText(file1Name);
+
+}
+
+void MainWindow::on_pushButton_run_clicked()
+{
+    std::cout << "Hello from pushButton_run \n";
 }
 
--- lui_testing/cpp_qt_toys/tst_demo/fit_n2/mainwindow.h
+++ lui_testing/cpp_qt_toys/tst_demo/fit_n2/mainwindow.h
@@ -15,6 +15,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_ini_clicked();
+
+    void on_pushButton_run_clicked();
+
 private:
     Ui::MainWindow *ui;
 };
