04fbb2b68ef4381633b3ade368de2de1d38a4691
luisodls <luis.fuentes-montero@diamond.ac.uk>
2024-11-22 20:31:39 +0000

    adding NOT C.O.R.S. simple python server

---
+++ lui_testing/py3_pyside2_n_dui2/http_tst/http_not_cors_server_tst.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from json import dumps
+
+""" The HTTP request handler """
+class RequestHandler(BaseHTTPRequestHandler):
+
+  def send_ok_dict(self):
+      '''used by both, GET or POST,'''
+      response = {}
+      response["status"] = "OK"
+      self.wfile.write(bytes(dumps(response), "utf8"))
+
+  def do_GET(self):
+      self.send_response(200)
+      self.end_headers()
+      self.send_ok_dict()
+
+  def do_POST(self):
+      self.send_response(200)
+      self.send_header("Content-Type", "application/json")
+      self.end_headers()
+
+      dataLength = int(self.headers["Content-Length"])
+      print("dataLength =", dataLength)
+      data = self.rfile.read(dataLength)
+      print("data =", data)
+      self.send_ok_dict()
+
+
+
+print("Starting server")
+
+ip_adr = "127.0.0.1"
+port_num = 45678
+httpd = HTTPServer((ip_adr, port_num), RequestHandler)
+full_url = "http://" + ip_adr + ":" + str(port_num)
+print("Hosting server on:", full_url )
+httpd.serve_forever()
