d688cb738a7f1b7069afdd4bb31535c86bca65dc
luisodls <luis.fuentes-montero@diamond.ac.uk>
2025-02-03 18:43:19 +0000

    duplicating a button, time to do a GET function

--- lui_testing/reactjs_toys/full_app_n2/button_n_stream_request.js
+++ lui_testing/reactjs_toys/full_app_n2/button_n_stream_request.js
@@ -5,7 +5,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { useState } from 'react';
 
-function MyButton({msgHere}) {
+function MyPostButton({msgHere}) {
   const [response, setResponse] = useState(null);
   const handleClick = async () => {
     try {
@@ -26,7 +26,33 @@ function MyButton({msgHere}) {
   }
   return (
     <button onClick={handleClick}>
-      Click me
+      Do POST
+    </button>
+  );
+}
+
+function MyGetButton({msgHere}) {
+  const [response, setResponse] = useState(null);
+  const handleClick = async () => {
+    try {
+      const res = await fetch("http://127.0.0.1:45678", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: msgHere }),
+      });
+      const data = await res.json();
+      setResponse(data);
+      console.log("data =", data);
+      alert(JSON.stringify(data));
+    } catch (error) {
+      console.error("Error:", error);
+      setResponse({ error: "Failed to connect to the server." });
+      console.log("res ERROR");
+    }
+  }
+  return (
+    <button onClick={handleClick}>
+      Do GET
     </button>
   );
 }
@@ -36,7 +62,7 @@ export default function Home() {
   const [usrName, setName] = useState('');
   return (
     <div>
-      <h1> Button and text connected </h1>
+      <h2> Two http Buttons </h2>
       <label>
         Give name here:
         <input
@@ -44,7 +70,8 @@ export default function Home() {
           onChange={e => setName(e.target.value)}
         />
       </label>
-      <MyButton msgHere ={usrName} />
+      <MyPostButton msgHere ={usrName} />
+      <MyGetButton msgHere ={usrName} />
     </div>
   );
 }
