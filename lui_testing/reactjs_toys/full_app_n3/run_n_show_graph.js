c34dbc876c0b55b275f0ff149d7c81d2d62cdf7f
luisodls <luis.fuentes-montero@@diamond.ac.uk>
2025-02-05 17:58:06 +0000

    organising and showing tree in the GUI

--- lui_testing/reactjs_toys/full_app_n3/run_n_show_graph.js
+++ lui_testing/reactjs_toys/full_app_n3/run_n_show_graph.js
@@ -5,6 +5,70 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { useState } from 'react';
 
+var tree_log = ["-- Do a GET to see the tree --"];
+
+function MyGetButton({ msgHere }) {
+  const [response, setResponse] = useState(null);
+  const handleClick = async () => {
+    try {
+      const path_in = new URLSearchParams({ msgHere }).toString();
+      const res = await fetch(`http://127.0.0.1:45678?${path_in}`, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+      });
+      const data = await res.json();
+      setResponse(data);
+      const my_lst = data.Answer;
+      console.log("Received Data:", my_lst);
+      tree_log = [" St lin# ", "__________________________"];
+      show_tree(0, my_lst, 1);
+    } catch (error) {
+      console.error("Fetch failed:", error.message);
+      setResponse({ error: "Failed to connect to the server." });
+    }
+  };
+  return (
+    <div>
+      <button onClick={handleClick}>
+        Do GET
+      </button>
+      {tree_log && (
+        <pre style={{ background: "#f4f4f4", padding: "10px", borderRadius: "5px" }}>
+          {JSON.stringify(tree_log, null, 2)}
+        </pre>
+      )}
+    </div>
+  );
+}
+
+function show_tree(pos_num, my_lst, indent = 1){
+    let step = my_lst[pos_num]
+    let stp_prn = "";
+    if( step.success === true ){
+        stp_prn = " T ";
+    }else if( step.success === false ){
+        stp_prn = " F ";
+    }else{
+        stp_prn = " N ";
+    }
+    let str_lin_num = String(step.lin_num);
+    stp_prn = stp_prn + "  " + str_lin_num + "      ".repeat(indent) + " └──";
+    try{
+        stp_prn = stp_prn + String(step.command)
+    } catch (error) {
+        stp_prn = stp_prn + "None"
+    }
+    tree_log.push(stp_prn);
+    console.log(stp_prn)
+    try{
+        for (let new_pos of step.nxt) {
+            show_tree(new_pos, my_lst, indent + 1);
+        }
+    } catch (error) {
+        console.log("last indent =", indent);
+    }
+}
+
 function MyPostButton({msgHere}) {
   const [response, setResponse] = useState(null);
   const handleClick = async () => {
@@ -27,66 +91,6 @@ function MyPostButton({msgHere}) {
     <button onClick={handleClick}>
       Do POST
     </button>
-  );
-}
-
-function show_tree(pos_num, my_lst, indent = 1){
-    let step = my_lst[pos_num]
-    let stp_prn = "";
-    if( step.success === true ){
-        stp_prn = " T ";
-    }else if( step.success === false ){
-        stp_prn = " F ";
-    }else{
-        stp_prn = " N ";
-    }
-    let str_lin_num = String(step.lin_num);
-    stp_prn = stp_prn + "  " + str_lin_num + "      ".repeat(indent) + " └──";
-    try{
-        stp_prn = stp_prn + String(step.command)
-    } catch (error) {
-        stp_prn = stp_prn + "None"
-    }
-    console.log(stp_prn)
-    try{
-        for (let new_pos of step.nxt) {
-            show_tree(new_pos, my_lst, indent + 1);
-        }
-    } catch (error) {
-        console.log("last indent =", indent);
-    }
-}
-
-function MyGetButton({ msgHere }) {
-  const [response, setResponse] = useState(null);
-  const handleClick = async () => {
-    try {
-      const path_in = new URLSearchParams({ msgHere }).toString();
-      const res = await fetch(`http://127.0.0.1:45678?${path_in}`, {
-        method: "GET",
-        headers: { "Content-Type": "application/json" },
-      });
-      const data = await res.json();
-      setResponse(data);
-      const my_lst = data.Answer;
-      console.log("Received Data:", my_lst);
-      show_tree(0, my_lst, 1);
-    } catch (error) {
-      console.error("Fetch failed:", error.message);
-      setResponse({ error: "Failed to connect to the server." });
-    }
-  };
-  return (
-    <div>
-      <button onClick={handleClick}>
-        Do GET
-      </button>
-      {response && (
-        <pre style={{ background: "#f4f4f4", padding: "10px", borderRadius: "5px" }}>
-          {JSON.stringify(response, null, 2)}
-        </pre>
-      )}
-    </div>
   );
 }
 
