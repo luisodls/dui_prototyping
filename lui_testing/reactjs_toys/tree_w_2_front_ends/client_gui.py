ab3866cc9ca94ffda1214c403d9e234a11f55d4a
luisodls <luis.fuentes-montero@diamond.ac.uk>
2025-02-24 19:17:16 +0000

    adding first graphics to isolated example of tree view

--- lui_testing/reactjs_toys/tree_w_2_front_ends/client_gui.py
+++ lui_testing/reactjs_toys/tree_w_2_front_ends/client_gui.py
@@ -4,6 +4,32 @@ from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from PySide2 import QtUiTools
 
+class TreeDirScene(QGraphicsScene):
+    tmp_off = '''
+    node_clicked_w_left = Signal(int)
+    node_clicked_w_right = Signal(int)
+    hide_clicked = Signal(int)
+    '''
+    def __init__(self, parent = None):
+        super(TreeDirScene, self).__init__(parent)
+        self.setFont(QFont("Courier"))
+        fm_rect = QFontMetrics(self.font()).boundingRect("W")
+        self.f_width = fm_rect.width()
+        self.f_height = fm_rect.height()
+        self.gray_pen = QPen(
+            Qt.gray, 2, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin
+        )
+        self.first_gray_brush = QBrush(Qt.gray, Qt.SolidPattern)
+
+    def draw_4_me(self, lst_out):
+        print("lst_out(draw_4_me) =", json.loads(lst_out))
+        self.addRect(
+            10, 10,
+            10, 10,
+            self.gray_pen, self.first_gray_brush
+        )
+        self.update()
+
 
 class Form(QObject):
     def __init__(self, parent = None):
@@ -14,7 +40,8 @@ class Form(QObject):
         self.window.Button4Post.clicked.connect(self.clicked_4_post)
         self.window.EditPostRequestLine.textChanged.connect(self.new_req_txt)
         self.req_qr = ""
-
+        self.tree_scene = TreeDirScene(self)
+        self.window.TreeGraphicsView.setScene(self.tree_scene)
         self.window.show()
 
     def clicked_4_get(self):
@@ -24,8 +51,7 @@ class Form(QObject):
             "http://127.0.0.1:45678", params = full_cmd
         )
         lst_out = req_get.content
-        print("lst_out =", json.loads(lst_out))
-
+        self.tree_scene.draw_4_me(lst_out)
 
     def clicked_4_post(self):
         print("time to do a http(Post) request with:", self.req_qr)
