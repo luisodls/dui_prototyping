7a17d59fd5c4382c7d4ad976c10abf878eb766d9
luisodls <luis.fuentes-montero@diamond.ac.uk>
2025-03-16 13:36:37 +0000

    testing local variables at Py3 style

--- lui_testing/type_script_n_O_O_P/tst_py3_style.ts
+++ lui_testing/type_script_n_O_O_P/tst_py3_style.ts
@@ -2,12 +2,15 @@
 class Greeter {
 
     who_2_greet: string;
+    local_num: number;
 
     constructor(name_in: string) {
         this.who_2_greet = name_in;
+        this.local_num = 5;
     }
 
     greet() {
+        console.log("local_num =", this.local_num)
         return `Hi there, ${this.who_2_greet}!`;
     }
 }
