e0e82ce11b7f7b8a7505b47777e8ae90b1f6f16a
luisolafg <luicub@gmail.com>
2025-09-02 08:17:26 +0100

    converting code exaple into a class

--- lui_testing/type_script_n_O_O_P/import_class_in_module/class_modl.ts
+++ lui_testing/type_script_n_O_O_P/import_class_in_module/class_modl.ts
@@ -1,5 +1,22 @@
 
-export function greet(name_in: string): string {
-  return "Hello ..." + name_in + "...nice to see you";
+// Define a simple class
+export class Greeter {
+    // Property
+    who_2_greet: string;
+
+    // Constructor
+    constructor(name_in: string) {
+        this.who_2_greet = name_in;
+    }
+
+    // Method
+    greet() {
+        return `Hi there, ${this.who_2_greet}!`;
+    }
 }
 
+// Create an instance of the class
+let greeter = new Greeter("Luiso's world");
+
+// Call the method and print the result
+console.log(greeter.greet());
--- lui_testing/type_script_n_O_O_P/import_class_in_module/main1.ts
+++ lui_testing/type_script_n_O_O_P/import_class_in_module/main1.ts
@@ -1,4 +1,8 @@
 
-import { greet } from "./class_modl";
+import { Greeter } from "./class_modl";
 
-console.log(greet("Luiso"));
+// Create an instance of the class
+let greeter = new Greeter("Luiso's world");
+
+// Call the method and print the result
+console.log(greeter.greet());
