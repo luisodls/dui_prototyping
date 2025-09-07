f496106194abed050d942eca0ed2fe0870fd23ac
luisodls <luis.fuentes-montero@diamond.ac.uk>
2025-09-07 21:12:43 +0100

    adding a more genral example of TS fft aproximation

--- lui_testing/python_mic/fourier_series_approximation.py
+++ lui_testing/python_mic/fourier_series_approximation.py
@@ -4,21 +4,24 @@ import numpy as np
 from scipy.integrate import quad
 import matplotlib.pyplot as plt
 
+pi = 3.14159235358
+
 # Editable function definition
 def f(x):
     return x # This function is used here
 
 # Interval
-L = np.pi
+L = 5.0
 
 # Number of Fourier terms
 N = 5
+
 
 # Compute a0
 def integrand_a0(x):
     return f(x)
 
-a0 = (1 / (2 * L)) * quad(integrand_a0, -L, L)[0]
+a0 = (1 / L) * quad(integrand_a0, 0, L)[0]
 
 print("a0 =", a0)
 
@@ -27,14 +30,14 @@ an = []
 bn = []
 
 def integrand_an(x, n):
-    return f(x) * np.cos(n * x)
+    return f(x) * np.cos(2 * pi * n * x / L)
 
 def integrand_bn(x, n):
-    return f(x) * np.sin(n * x)
+    return f(x) * np.sin(2 * pi * n * x / L)
 
 for n in range(1, N + 1):
-    an_val = (1 / L) * quad(integrand_an, -L, L, args=(n,))[0]
-    bn_val = (1 / L) * quad(integrand_bn, -L, L, args=(n,))[0]
+    an_val = (2 / L) * quad(integrand_an, 0, L, args=(n,))[0]
+    bn_val = (2 / L) * quad(integrand_bn, 0, L, args=(n,))[0]
     an.append(an_val)
     bn.append(bn_val)
 
@@ -45,11 +48,11 @@ print("bn=\n", bn, "\n")
 def fourier_approx(x):
     sum = a0
     for n in range(1, N + 1):
-        sum += an[n-1] * np.cos(n * x) + bn[n-1] * np.sin(n * x)
+        sum += an[n-1] * np.cos(2 * pi * n * x / L) + bn[n-1] * np.sin(2 * pi * n * x / L)
     return sum
 
 # Generate x values and compute approximations
-x_vals = np.arange(-L, L, 0.1)
+x_vals = np.arange(0, L, 0.1)
 
 f_vals = []
 for x in x_vals:
@@ -60,8 +63,6 @@ for x in x_vals:
     approx_y = fourier_approx(x)
     approx_vals.append(approx_y)
     print("x =", x,"f_approx =", approx_y)
-
-
 
 # Plotting
 plt.plot(x_vals, f_vals, label='Original Function', color='blue')
--- lui_testing/type_script_n_O_O_P/fft_general.ts
+++ lui_testing/type_script_n_O_O_P/fft_general.ts
@@ -7,16 +7,16 @@ let pi = 3.14159265358;
 let n = 5;
 let step = 0.001;
 
-let interval_from = -pi;
-let interval_to = pi;
-let diff_L = (interval_to - interval_from) / 2;
+let interval_from = 0;
+let interval_to = 5;
+let diff_L = interval_to - interval_from;
 
 function summation_a_0(x_min: number, x_max: number): number {
   let sum = 0;
   for (let x = x_min; x < x_max; x += step) {
     sum +=  f(x) * step;
   }
-  sum = ( 1 / (2 * diff_L) ) * sum;
+  sum = ( 1 / diff_L) * sum;
   return sum;
 }
 let a0 = summation_a_0(interval_from, interval_to);
@@ -28,11 +28,11 @@ function summation_an_p_bn(
   let sum_a = 0;
   let sum_b = 0;
   for (let x = x_min; x < x_max; x += step) {
-    sum_a +=  f(x) * Math.cos(n * x) * step;
-    sum_b +=  f(x) * Math.sin(n * x) * step;
+    sum_a +=  f(x) * Math.cos(2 * pi * n * x / diff_L) * step;
+    sum_b +=  f(x) * Math.sin(2 * pi * n * x / diff_L) * step;
   }
-  sum_a = ( 1 / diff_L ) * sum_a;
-  sum_b = ( 1 / diff_L ) * sum_b;
+  sum_a = ( 2 / diff_L ) * sum_a;
+  sum_b = ( 2 / diff_L ) * sum_b;
   return [sum_a, sum_b];
 }
 
@@ -49,8 +49,8 @@ console.log("bn =", bn)
 function fourier_approx(x: number): number {
     let sum = a0;
     for (let num: number = 1; num < n + 1; num++) {
-        sum += an[num - 1] * Math.cos((num * x * pi) / diff_L)
-            +  bn[num - 1] * Math.sin((num * x * pi) / diff_L);
+        sum += an[num - 1] * Math.cos(2 * pi * num * x / diff_L)
+            +  bn[num - 1] * Math.sin(2 * pi * num * x / diff_L);
     }
     return sum;
 }
