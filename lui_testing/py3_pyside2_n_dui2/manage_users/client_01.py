4724e39642bb802c8ab61bd0325b86f0665f8029
Luis Fuentes-Montero <luis.fuentes-montero@diamond.ac.uk>
2025-09-30 17:13:35 +0100

    adding post feature to copy/pasted example

--- lui_testing/py3_pyside2_n_dui2/manage_users/client_01.py
+++ lui_testing/py3_pyside2_n_dui2/manage_users/client_01.py
@@ -1,4 +1,4 @@
-import sys, os
+import sys, os, requests, json
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
@@ -14,26 +14,36 @@ class Form(QObject):
         self.window = QtUiTools.QUiLoader().load("simple.ui")
 
         self.window.Button1.clicked.connect(self.clicked)
-        self.html_view = QWebEngineView()
-
         main_box = QVBoxLayout()
-        main_box.addWidget(self.html_view)
-        #main_box.addWidget(QLabel("Testing"))
+
+        self.text_out = QTextBrowser()
+        main_box.addWidget(self.text_out)
 
         self.window.InerWidget.setLayout(main_box)
         self.window.show()
 
     def clicked(self):
         print("clicked")
-        self.html_view.load(QUrl("http://127.0.0.1:45678"))
 
-        '''self.html_view.load(
-            QUrl.fromLocalFile(
-                "/tmp/run_dui2_nodes/run4/dials.report.html"
+
+        cmd = {"cmd_lst":["a1", "a1"]}
+        print("cmd =", cmd)
+        try:
+            full_cmd = {"message":"something here"}
+            req_post = requests.post(
+                "http://127.0.0.1:45678", data = json.dumps(full_cmd)
             )
-        )'''
+            lst_out = req_post.content
+            print("lst_out =", json.loads(lst_out))
 
-        self.html_view.show()
+        except requests.exceptions.RequestException:
+            print(
+                "something went wrong with the << reset_graph >> request"
+            )
+
+        print("CommadLineEdit =", str(self.window.CommadLineEdit.text()))
+        print("Data_LineEdit_01 =", str(self.window.Data_LineEdit_01.text()))
+        print("Data_LineEdit_02 =", str(self.window.Data_LineEdit_02.text()))
 
 
 if __name__ == '__main__':
