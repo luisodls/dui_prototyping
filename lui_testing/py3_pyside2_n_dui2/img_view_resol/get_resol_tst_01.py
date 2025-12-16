753e04b93ad4b58c17a6bd85ccc9ba3c1931fd18
luisodls <luis.fuentes-montero@@diamond.ac.uk>
2025-12-16 13:48:22 +0000

    starting point example for resolution calculation

---
+++ lui_testing/py3_pyside2_n_dui2/img_view_resol/get_resol_tst_01.py
@@ -0,0 +1,44 @@
+from dxtbx.model import ExperimentList
+
+
+def get_resolution(detector, experiments, x, y, readout=None):
+    """
+    Determine the resolution of a pixel.
+    Arguments are in image pixel coordinates (starting from 1,1).
+
+    this is copy/pasted from Dials Image Viewer
+    as a starting point for testing
+    """
+
+    #detector = self.raw_image.get_detector()
+    #beam = self.raw_image.get_beam()
+
+    beam = experiments.beams()[0]
+
+    if detector is None or beam is None:
+        return None
+
+    if readout is None:
+        return None
+
+    panel = detector[readout]
+
+    if abs(panel.get_distance()) > 0:
+        return panel.get_resolution_at_pixel(beam, (x, y))
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    experiments_path = "/tmp/tst4dials_img_view/imported.expt"
+
+    experiments = ExperimentList.from_file(experiments_path)
+    my_imageset = experiments.imagesets()[0]
+    detector = my_imageset.get_detector()
+
+    print("importing from:", experiments_path)
+
+    resolution = get_resolution(detector, experiments, 900, 900, readout = 0)
+
+    print("resolution =", resolution)
+
