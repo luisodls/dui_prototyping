7db2c9ccbb637925097f4d36b2f8bf1273500e15
luisodls <luis.fuentes-montero@@diamond.ac.uk>
2026-01-26 17:56:31 +0000

    playing with label update

--- lui_testing/py3_pyside2_n_dui2/prog_dialog/test01.py
+++ lui_testing/py3_pyside2_n_dui2/prog_dialog/test01.py
@@ -16,6 +16,33 @@ except ModuleNotFoundError:
     print("Using PySide2 as Qt bindings")
 
 
+class Progress_Box(QDialog):
+    def __init__(self, parent = None):
+        super(Progress_Box, self).__init__(parent)
+
+        Stop_Butn = QPushButton("Stop ...")
+        Stop_Butn.clicked.connect(self.stop_me)
+
+        self.live_label = QLabel("progress")
+        self.num_in_label = 0
+
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.live_label)
+        mainLayout.addWidget(Stop_Butn)
+        self.setLayout(mainLayout)
+        self.setWindowTitle("Loading")
+
+        timer = QTimer(self)
+        timer.timeout.connect(self.update_me)
+        timer.start(500)
+
+    def stop_me(self):
+        print("time to stop")
+
+    def update_me(self):
+        self.num_in_label += 5
+        self.live_label.setText(str(self.num_in_label))
+
 class Form(QObject):
     def __init__(self, parent = None):
         super(Form, self).__init__(parent)
@@ -26,11 +53,15 @@ class Form(QObject):
         main_box = QVBoxLayout()
         main_box.addWidget(QLabel("Testing"))
 
+        self.p_box = Progress_Box()
+
         self.window.InerWidget.setLayout(main_box)
         self.window.show()
 
     def clicked(self):
         print("clicked")
+        self.p_box.show()
+
 
 if __name__ == '__main__':
     os.environ["QTWEBENGINE_CHROMIUM_FLAGS"] = "--no-sandbox"
