e0a0b137bb52bd7a4d5ce88a19d2598ec2c1cba9
luisodls <luis.fuentes-montero@diamond.ac.uk>
2026-03-23 21:40:25 +0000

    test first similar example with vite, now in JSX

---
+++ lui_testing/road_2_react_toys/jsx-app-01/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>My App #2</h1>
+        </div>
+        <button
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
--- lui_testing/road_2_react_toys/tsx-app-01/src/App.tsx
+++ lui_testing/road_2_react_toys/tsx-app-01/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 
   return (
     <div>
-    <h1>My App</h1>
+    <h1>My App #1</h1>
     <button onClick={() => setCount(count + 1)}>
     Clicked {count} times
     </button>
