ea4cb8c9769ac32b2e18669cb20ae9e0319d2285
luisodls <luis.fuentes-montero@diamond.ac.uk>
2026-03-23 21:20:49 +0000

    test first example with vite

---
+++ lui_testing/road_2_react_toys/tsx-app-01/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+/*
+ * when we trim down to a simpler app the next 4 imports are no longer needed
+
+ import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+*/
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <h1>My App</h1>
+    <button onClick={() => setCount(count + 1)}>
+    Clicked {count} times
+    </button>
+    </div>
+  )
+}
+
+export default App
