7049f883addb0257462628335eff06781be267d2
luisodls <luis.fuentes-montero@@diamond.ac.uk>
2026-03-25 15:30:39 +0000

    removing commented leftover for simplification

--- lui_testing/road_2_react_toys/tsx-app-01/src/App.tsx
+++ lui_testing/road_2_react_toys/tsx-app-01/src/App.tsx
@@ -1,13 +1,4 @@
 import { useState } from 'react'
-
-/*
- * when we trim down to a simpler app the next 4 imports are no longer needed
-
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-*/
-
 import './App.css'
 
 function App() {
