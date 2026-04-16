bfbf7f40362379cc90b858159143efc6636ed8fc
luisodls <luis.fuentes-montero@diamond.ac.uk>
2026-04-16 23:50:08 +0100

    fixing issue with space in parameter list

--- lui_testing/xia2_reading/reading_toy_03_n_building_conetor.py
+++ lui_testing/xia2_reading/reading_toy_03_n_building_conetor.py
@@ -11,19 +11,24 @@ def get_list_of_commands(path_in):
             print("\n")
             new_cmd_str = lines_str[position + 1][1:-1]
             print("<<", new_cmd_str, ">> \n")
-            per_line_cmd_lst = new_cmd_str.split(" ")
 
-            full_cmd_lst = []
+            divide_pos = new_cmd_str.find("'")
+
+            exe_cmd = new_cmd_str[0:divide_pos]
+            print("exe_cmd =", exe_cmd)
+            per_line_cmd_lst = new_cmd_str[divide_pos + 1:-1].split("' '")
+            print("per_line_cmd_lst =", per_line_cmd_lst)
+
+            full_cmd_lst = [exe_cmd]
+
             for inner_cmd in per_line_cmd_lst:
                 if inner_cmd == "":
                     pass
 
-                elif inner_cmd[0] == "'":
-                    full_cmd_lst.append(inner_cmd[1:-1])
-
-                else:
-                    exe_cmd = inner_cmd
+                elif inner_cmd not in full_cmd_lst:
                     full_cmd_lst.append(inner_cmd)
+
+            print("full_cmd_lst = ", full_cmd_lst)
 
             if exe_cmd == 'dials.report':
                 print('exe_cmd = dials.report')
@@ -52,6 +57,7 @@ def get_list_of_commands(path_in):
                     single_par not in connect_for_next_lst
                 ):
                     connect_from_prev_lst.append(single_par)
+
             tuning_params_lst = []
             for single_par in full_cmd_lst:
                 if(
@@ -61,28 +67,17 @@ def get_list_of_commands(path_in):
                 ):
                     tuning_params_lst.append(single_par)
 
-            off_for_now = '''
             print("connect_from_prev_lst =", connect_from_prev_lst, "\n")
             print("connect_for_next_lst =", connect_for_next_lst, "\n")
             print("full_cmd_lst =", full_cmd_lst, "\n")
-            '''
 
             cmd_dict = {
                 'full_cmd_lst'              :full_cmd_lst,
                 'exe_cmd'                   :exe_cmd,
                 'connect_from_prev_lst'     :connect_from_prev_lst,
-                'connect_for_next_lst'       :connect_for_next_lst,
-                'tuning_params_lst'              :tuning_params_lst,
+                'connect_for_next_lst'      :connect_for_next_lst,
+                'tuning_params_lst'         :tuning_params_lst,
             }
-            if len(cmd_dict['exe_cmd']) > 6:
-                if cmd_dict['exe_cmd'][0:6] == "dials.":
-                    cmd_dict['dials_cmd'] = cmd_dict['exe_cmd'][6:]
-
-                else:
-                    cmd_dict['dials_cmd'] = False
-
-            else:
-                cmd_dict['dials_cmd'] = False
 
             list_of_commands.append(cmd_dict)
 
@@ -100,11 +95,12 @@ def main():
     else:
         lst_cmd = get_list_of_commands(arg_in)
 
-    off_for_now = '''
+    #off_for_now = '''
     for pos_num, command in enumerate(lst_cmd):
         print("\n num =", pos_num, "\n", command, "\n")
-    '''
+    #'''
 
+    tmp_off = '''
     for pos_num, command in enumerate(lst_cmd):
         print(
             "\n num=<<", pos_num, ">>\nexe_cmd=<<", command["exe_cmd"],
@@ -112,6 +108,7 @@ def main():
         )
 
         print(command)
+    '''
 
 
 if __name__ == "__main__":
