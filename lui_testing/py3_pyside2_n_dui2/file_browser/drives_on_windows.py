0d9698b68f54883cd359c264d95b2a174c22467c
luisodls <luis.fuentes-montero@diamond.ac.uk>
2026-05-14 10:59:56 +0100

    first reliable prototype to find drives on a windows machine, using a bit of a brute force

--- lui_testing/py3_pyside2_n_dui2/file_browser/drives_on_windows.py
+++ lui_testing/py3_pyside2_n_dui2/file_browser/drives_on_windows.py
@@ -1,8 +1,23 @@
+only_shows_C = '''
 import psutil
 drives = [drive.device for drive in psutil.disk_partitions()]
 print(drives)
+'''
 
-list_directily_C = '''
+lists_directily_C = '''
 import os
 print(os.listdir("/"))
 '''
+
+import os
+
+list_of_posible_drives = [
+    'A:','B:','C:','D:','E:','F:','G:','H:','I:','J:','K:','L:','M:'
+    ,'N:','O:','P:','Q:','R:','S:','T:','U:','V:','W:','X:','Y:','Z:'
+]
+list_of_all_real_drives = []
+for tst_let in list_of_posible_drives:
+    if os.path.isdir(tst_let):
+        list_of_all_real_drives.append(tst_let)
+
+print("list of all real drives = ", list_of_all_real_drives)
